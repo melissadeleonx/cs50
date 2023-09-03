69ba3bd0737caef44058ab14e301083eb668adc2
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-03 17:07:08 +0000

    Sun, Sep 3, 2023, 7:07 PM +02:00

--- compare.c
+++ compare.c
@@ -9,13 +9,13 @@ int main(void)
     if (x < y)
     {
         printf("x is less than y\n");
-        }
-        else if (x > y)
-        {
+    }
+    else if (x > y)
+    {
             printf("x is greater than y\n");
-        }
-        else
-        {
+    }
+    else
+    {
             printf("x is equal to y\n");
-        }
+    }
 }
