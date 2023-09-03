b137aa4605d97e6438865714d3bbc8b181f2eccd
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-03 13:08:47 +0000

    Sun, Sep 3, 2023, 3:08 PM +02:00

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@
 
 int main(void)
 {
-    string answer = get_string("What is your name? ")string answer = get_string("What is your name? ")
+    string answer = get_string("What is your name? ");
     printf("hello, answer\n");
     }
