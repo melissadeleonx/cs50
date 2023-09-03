28dbedba82e0deaafd97e91725efb3df5b007547
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-03 16:25:24 +0000

    Sun, Sep 3, 2023, 6:25 PM +02:00

--- hello.c
+++ hello.c
@@ -6,11 +6,14 @@ int main(void)
     string answer = get_string("What's your name? ");
     printf("hello, %s!\n", answer);
     }
-    if (x){
+    if (x < y){
 
     }
-    else
+    else if (x > y)
     {
 
+    }
+    else if (x == y)
+    {
 
     }
