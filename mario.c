893fc36c5d983e0538ac1105ee4e71863406c7e9
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-04 23:44:53 +0000

    Tue, Sep 5, 2023, 1:44 AM +02:00

--- mario.c
+++ mario.c
@@ -2,11 +2,11 @@
 #include <cs50.h>
 int main(void)
 {
-    int n = get_int("Size: ")
+    int n = get_int("Size: ");
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++) {
-             printf("# ");
+             printf("#");
         }
         printf("\n");
     }
