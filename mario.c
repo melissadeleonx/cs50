a80f3c69267d3ea9e48bb1b63d46e79506d94224
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-04 23:38:54 +0000

    Tue, Sep 5, 2023, 1:38 AM +02:00

--- mario.c
+++ mario.c
@@ -2,8 +2,9 @@
 
 int main(void)
 {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    const int n = 3;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             printf("# ");
         }
         printf("\n");
