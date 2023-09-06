bba1cddac1c7ec0f64230a4f221c23e4c8d13cf9
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-06 14:27:36 +0000

    Wed, Sep 6, 2023, 4:27 PM +02:00

--- marionew.c
+++ marionew.c
@@ -49,11 +49,11 @@ int get_size(void)
 void print_grid(int size)
  {
     for (int i = 0; i < size; i++)
-    {
+ {
     for (int j = 0; j < size; j++)
     {
         printf("#");
-    }
-            printf("\n");
         }
-    }
+        printf("\n");
+        }
+}
