f1e6bd56fe43aa27dfb2c6d123df67b88c220663
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-06 12:56:25 +0000

    Wed, Sep 6, 2023, 2:56 PM +02:00

--- mario.c
+++ mario.c
@@ -11,8 +11,7 @@ int main(void)
     while (n < 1);
     for (int i = 0; i < n; i++)
     {
-    // Print grid of bricks
-        for (int j = 0; j < n; j++) {
+    for (int j = 0; j < n; j++) {
              printf("#");
         }
         printf("\n");
