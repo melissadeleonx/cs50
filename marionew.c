d5480d1f9b7bf7e20dda3635c34c711b91aaef39
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-06 13:47:05 +0000

    Wed, Sep 6, 2023, 3:47 PM +02:00

---
+++ marionew.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Get size of grid
+    int n = get_size();
+    // Print grid of bricks
+    printgrid();
+}
+
+int get_size(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+    return n;
+
+}
