8706c338dd172740d0414475f99209d1b18f78cb
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-07 10:19:32 +0000

    Thu, Sep 7, 2023, 12:19 PM +02:00

--- phonebook.c
+++ phonebook.c
@@ -2,3 +2,13 @@
 #include <stdio.h>
 
 int main(void)
+
+// Maximum Contact lis
+
+// Make contact list
+
+// Add contact, Name, Phone Number, Email Address, Address
+
+// Edit Contact
+
+// Delete a contact
