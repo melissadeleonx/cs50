2f68e41e063a63c2b266e2c9005b2b419d629ecf
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-07 16:14:58 +0000

    Thu, Sep 7, 2023, 6:14 PM +02:00

--- phonealt.c
+++ phonealt.c
@@ -7,5 +7,5 @@ int main(void)
     int age = get_int("What's your age? ");
     string number = get_string("What's your phone number? ");
 
-    printf("Age is %i". "Name is %s". "Phone Number is %s.", age, name, number);
+    printf("Age is %i". "Name is %s". "Phone Number is %s..\n", age, name, number);
 }
