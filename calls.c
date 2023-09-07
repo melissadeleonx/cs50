34449fb5840f3f6e9af8226f6ff53970b79a59e5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-07 12:54:49 +0000

    Thu, Sep 7, 2023, 2:54 PM +02:00

--- calls.c
+++ calls.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 
-int main() {
-    int calls; // Declare an integer variable to store user input
+int main(void)
+{
+int calls = 4;
+printf("calls is %d\n", calls)
 
-    // Display a message to the user
-    printf("Please enter the number of calls: ");
-
-    // Read an integer input from the user
-    scanf("%d", &calls);
-
-    // Display a message using the user's input
-    printf("You entered %d calls.\n", calls);
-
-    return 0;
 }
