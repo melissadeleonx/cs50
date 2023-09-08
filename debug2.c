666a454c96668b149cd8a64aeb7b964f6cbfc19f
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-08 13:09:26 +0000

    Fri, Sep 8, 2023, 3:09 PM +02:00

---
+++ debug2.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height, width = get_int("height: ");
+                        get_int("weight: ");
+
+    printf("Your height is %i and your weight is %i\n, height, width);
+
+    return 0;
+}
