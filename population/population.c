5933bc469529d7260e8f6b1636519dc3153e8882
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-08 16:06:03 +0000

    Fri, Sep 8, 2023, 6:06 PM +02:00

--- population/population.c
+++ population/population.c
@@ -3,20 +3,20 @@
 
 int main(void)
 {
-    // TODO: Prompt for start size
     int n = get_int("start: ");
     int x = get_int("first: ");
     int y = get_int("second: ");
 
+    // TODO: Calculate number of years until we reach threshold
+
     int end = n + x - y;
+
+    // TODO: Print number of years
 
     printf("The number of llamas at the end of the year is %i\n", end);
 
     return 0;
-
+    
     // TODO: Prompt for end size
 
-    // TODO: Calculate number of years until we reach threshold
-
-    // TODO: Print number of years
 }
