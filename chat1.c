c241469dd3b589d1fc8cc075479af306eea39385
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-08 13:34:19 +0000

    Fri, Sep 8, 2023, 3:34 PM +02:00

--- chat1.c
+++ chat1.c
@@ -3,13 +3,16 @@
 
 int main(void)
 {
-    int num1 = 10;
-    int num2 = 0;
-    string result;
+    int num1 = get_int("num1: ");
+    int num2 = get_int("num2: ");
+    int result;
 
-    result = num1 / num2;
-
-    printf("Division of 0 is not allowed\n", result);
-
+    if (num1 != 0 && num2 != 0)
+    {
+        result = num1 / num2;
+        printf("The result is: %i\n", result);
+    } else {
+        printf("Division by zero is not allowed\n");
+        }
     return 0;
 }
