d22c81e9f0cad0b6166179196c1b907c4f543990
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-08 07:41:53 +0000

    Fri, Sep 8, 2023, 9:41 AM +02:00

--- debug.c
+++ debug.c
@@ -7,16 +7,15 @@ int main(void)
     int y;
     float result;
 
-    // print and scan x
+    float result = x / y;
+
     printf("x: ");
     scanf("%i", x);
 
-    // print and scan y
     printf("y: ");
     scanf("%i", y);
 
     if (x || y != 0) {
-        result = x / y;
         printf("The result of %d and %d is: %.2f\n", x, y, result);
     } else {
         printf("Division by zero is not allowed.\n");
