55f04580ecd9262596c8c3527f840179c0ae5e20
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-08 07:34:12 +0000

    Fri, Sep 8, 2023, 9:34 AM +02:00

--- debug.c
+++ debug.c
@@ -1,13 +1,15 @@
+#include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
-    int num1 = 10;
-    int num2 = 0;
-    int result;
+int main(void)
+{
+    int x = 
+    int y =
+    int result =
 
-    
 
-    if (num2 != 0) {
+
+    if (x || y != 0) {
         result = num1 / num2;
         printf("The result of %d and %d is: %.d\n", num1, num2, result);
     } else {
