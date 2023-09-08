339c437e933485b40aac9976044479336bfa297a
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-08 23:54:51 +0000

    Sat, Sep 9, 2023, 1:54 AM +02:00

--- chat1.c
+++ chat1.c
@@ -3,30 +3,30 @@
 
 int main(void)
 {
-
-    do {
-        int n = get_int("Enter a starting population size : ");
-
+    int n;
+    do
+    {
+        n = get_int("Enter a starting population size : ");
+        
         if (n < 9)
         {
-            get_int("Enter a starting population size (must be >= 9): ");
-            printf("Enter a starting population size (must be >= 9): \n");
+            printf("Enter a starting population size (must be >= 9): ");
         }
     while (n < 9);
 
     int x = get_int("Number of newborn llamas: ");
 
-    int y = get_int("Number of deceased llamas: ");
-
-    if (y > n)
-        {
-            printf("Number of deceased llamas: (must be less than the starting population) ");
-        }
-        }
+    int y;
+    do
+    {
+        y = get_int("Number of deceased llamas: (must be <= n): ");
+    }
+    while (y < n);
 
     int end = n + x - y;
 
     printf("The number of llamas at the end of the year is %i\n", end);
 
     return 0;
+}
 }
