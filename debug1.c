aae40e56d52edd2babc0963abccaf98426b929c8
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-08 08:17:28 +0000

    Fri, Sep 8, 2023, 10:17 AM +02:00

--- debug1.c
+++ debug1.c
@@ -5,9 +5,12 @@ int main(void)
 {
     float num1 = get_float("insert num1: ");
     float num2 = get_float("insert num2: ");
-    float difference;
+    float difference = num1 - num2;
 
-    difference = num1 - num2;
+    if (difference < 0)
+    {
+    printf("The difference of %.2f and %.2f is: %.2f (negative)\n", num1, num2, difference);
+    }
     printf("The difference of %.2f and %.2f is: %.2f\n", num1, num2, difference);
 
     return 0;
