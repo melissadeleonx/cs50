245cb7242d8ce0c4315ea5cf02d833cd4686d22a
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-08 08:02:10 +0000

    Fri, Sep 8, 2023, 10:02 AM +02:00

---
+++ debug1.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 15;
+    int num2 = 7;
+    int difference;
+
+    difference = num1 - num2;
+    printf("The difference is: %d\n", num1, num2);
+
+    return 0;
+}
