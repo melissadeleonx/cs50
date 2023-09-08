de4c101a8353f666ff4ed3780d737d002ff42b9d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-08 08:08:07 +0000

    Fri, Sep 8, 2023, 10:08 AM +02:00

--- debug1.c
+++ debug1.c
@@ -3,12 +3,12 @@
 
 int main(void)
 {
-    int num1 = 15
-    int num2 = 7;
+    int num1 = get_int("insert num1: ");
+    int num2 = get_int("insert num2: ");
     int difference;
 
     difference = num1 - num2;
-    printf("The difference is: %d\n", difference);
+    printf("The difference of %i and %i is: %d\n", num1, num2, difference);
 
     return 0;
 }
