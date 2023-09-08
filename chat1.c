f601c4df529c7402544bfe666747833d6e75bb99
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-08 13:14:14 +0000

    Fri, Sep 8, 2023, 3:14 PM +02:00

---
+++ chat1.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 10;
+    int num2 = 0;
+    int result;
+
+    result = num1 / num2;
+
+    printf("The result is: %d\n", result);
+
+    return 0;
+}
