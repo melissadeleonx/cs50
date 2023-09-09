34b27d244b1d2d8702c8f412489bf726c3c4fef8
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-09 12:16:09 +0000

    Sat, Sep 9, 2023, 2:16 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,14 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-Iterate from 1 through height:
-On iteration i, print i hashes and then a newline
 int main(void)
 {
-    // Prompt user for height
+        // Prompt user for height
+    int height;
+    // conditionals: if height < 1 or > 8 (or not a positive integer at all), prompt again and again (loop) until valid answer is received
+    do {
+        height = get_int ("Height: ");
 
-// conditionals: if height < 1 or > 8 (or not a positive integer at all), prompt again and again (loop) until valid answer is received
+        } while height > 1 && height < 8
 
-// Add
+
+
+// Add Iterate from 1 through height: On iteration i, print i hashes and then a newline
 
 }
