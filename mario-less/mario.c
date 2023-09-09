be9534deeb1fe52605473f13f7a0e95af0c3cf81
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-09 12:09:32 +0000

    Sat, Sep 9, 2023, 2:09 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,7 +3,7 @@
 
 // Prompt user for height
 
-// conditionals: if height < 1 or > 8 (or not an integer at all), prompt again and again until valid answer is received
+// conditionals: if height < 1 or > 8 (or not an integer at all), prompt again and again (loop) until valid answer is received
 
 Iterate from 1 through height:
 On iteration i, print i hashes and then a newline
