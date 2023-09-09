0eb9e2c5b845aca17c9755d553f5e5f4551fcda5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-09 12:26:34 +0000

    Sat, Sep 9, 2023, 2:26 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -9,5 +9,6 @@ int main(void)
          get_int("Height: ");
         } while (height > 1);
           while (height < 8);
+        
 // Add Iterate from 1 through height: On iteration i, print i hashes and then a newline
 }
