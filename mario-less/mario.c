23204490d5759bf3c58f065437795ba736dc7f00
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-09 12:22:25 +0000

    Sat, Sep 9, 2023, 2:22 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,13 +4,11 @@
 int main(void)
 {
         // Prompt user for height
-    int height;
+    int height = get_int("Height: ");
     // conditionals: if height < 1 or > 8 (or not a positive integer at all), prompt again and again (loop) until valid answer is received
     do {
-        if (height > 1);
-        height = get_int("Height: ");
+         get_int("Height: ");
         } while (height > 1);
-
 // Add Iterate from 1 through height: On iteration i, print i hashes and then a newline
 
 }
