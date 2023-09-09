ba8bc7301df9138c0c2143878934bde9f410028b
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-09 16:39:56 +0000

    Sat, Sep 9, 2023, 6:39 PM +02:00

--- half/half.c
+++ half/half.c
@@ -5,8 +5,8 @@ int main(void)
 {
     float bill_amount = get_float("Bill before tax and tip: ");
     float tax_percent = get_float("Sale Tax Percent: ");
-    float billtax = ((tax_percent / 100) * bill_amount);
-    float bill_amount_with_tax = (bill_amount + tax_percent);
+    float tax_amount = ((tax_percent / 100) * bill_amount);
+    float bill_amount_with_tax = (bill_amount + tax_amount);
     int tip_percent = get_int("Tip percent: ");
     float billtip = ((tip_percent / 100) * bill_amount_with_tax);
     int people = get_int("How many are you: ");
