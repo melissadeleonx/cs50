1b22dc5876c927b18916a827d627b49262b35119
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-09 07:26:38 +0000

    Sat, Sep 9, 2023, 9:26 AM +02:00

--- chat1.c
+++ chat1.c
@@ -2,26 +2,7 @@
 #include <stdio.h>
 
 int main(void)
+
 {
-    int n;
-
-    do
-    {
-        n = get_int("Enter a starting population size : ");
-        if (n < 9)
-        {
-            printf("Enter a starting population size (must be >= 9): ");
-        }
-    while (n < 9);
-    int x = get_int("Number of newborn llamas: ");
-
-    int y;
-
-    y = get_int("Number of deceased llamas: (must be <= n): ");
-
-    int end = n + x - y;
-
-    printf("The number of llamas at the end of the year is %i\n", end);
-
-    return 0; }
+    
 }
--- population/population.c
+++ population/population.c
@@ -3,28 +3,17 @@
 
 int main(void)
 {
-    int n;
-
+    int n = get_int("Enter a starting population size : ");
+    int x = get_int("Number of newborn llamas: ");
+    int y = get_int("Number of deceased llamas: ");
+    int end = n + x - y;
+    printf("The number of llamas at the end of the year is %i\n", end);
     do
     {
-        n = get_int("Enter a starting population size : ");
         if (n < 9)
         {
             printf("Enter a starting population size (must be >= 9): ");
         }
     while (n < 9);
-    int x = get_int("Number of newborn llamas: ");
-
-    int y;
-    do
-    {
-        y = get_int("Number of deceased llamas: (must be <= n): ");
-    }
-    while (y < n);
-
-    int end = n + x - y;
-
-    printf("The number of llamas at the end of the year is %i\n", end);
-
     return 0; }
 }
