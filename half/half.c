6f19a7ecc3b84362e726db11bd55e5de9d40be2d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-09 22:19:34 +0000

    Sun, Sep 10, 2023, 12:19 AM +02:00

--- half/half.c
+++ half/half.c
@@ -1,22 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Function to calculate the total amount with tax
+float calculateTotalWithTax(float billAmount, float taxRate)
+{
+    return billAmount + (taxRate / 100) * billAmount;
+}
+
+// Function to calculate the tip amount
+float calculateTip(float totalAmount, float tipPercent)
+{
+    return (tipPercent / 100) * totalAmount;
+}
+
 int main(void)
 {
-    float bill_amount = get_float("Bill before tax and tip: ");
-    float tax_percent = get_float("Sale Tax Percent: ");
-    // get the amount of the %
-    float tax_amount = ((tax_percent / 100) * bill_amount);
-    // get the bill amount + tax
-    float bill_amount_with_tax = (bill_amount + tax_amount);
-    // get the amount of the tip percent
-    int tip_percent = get_int("Tip percent: ");
-    float tip_amount = ((tip_percent / 100) * bill_amount_with_tax);
+    float billAmount = get_float("Bill before tax and tip: ");
+    float taxRate = get_float("Sales Tax Percent: ");
+    float totalAmount = calculateTotalWithTax(billAmount, taxRate);
+
+    float tipPercent = get_float("Tip percent: ");
+    float tipAmount = calculateTip(totalAmount, tipPercent);
 
     int people = get_int("How many are you: ");
-    float my_half = ((bill_amount_with_tax + tip_amount) / people);
+    float perPersonAmount = (totalAmount + tipAmount) / people;
 
-    printf("You will owe $%.2f each!\n", my_half);
+    printf("Each person will owe $%.2f\n", perPersonAmount);
 
     return 0;
 }
