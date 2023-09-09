90610154223199ba01faff7e1e1c9cc1842c4cf5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-09 07:54:19 +0000

    Sat, Sep 9, 2023, 9:54 AM +02:00

--- chat1.c
+++ chat1.c
@@ -6,7 +6,11 @@ int main(void)
     int n;
 
     do {
-        n = get_int("Enter a starting population size (greater than 9): ");
+        n = get_int("Enter a starting population size: ");
+
+        if (n <= 9) {
+            printf("Please enter a starting population size greater than 9.\n");
+        }
     } while (n <= 9);
 
     int x = get_int("Number of newborn llamas: ");
