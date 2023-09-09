5e5429e526be6d1ce2d3e919dfcbdef4eecd5ca0
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-09 07:41:49 +0000

    Sat, Sep 9, 2023, 9:41 AM +02:00

--- chat1.c
+++ chat1.c
@@ -4,14 +4,6 @@
 int main(void)
 {
     int n = get_int("Enter a starting population size : ");
-    while (n < 9) { if (n < 9)
-    {
-        get_int("Enter a starting population size not less than 9: ");
-        }
-        else {
-        get_int("Enter a starting population size : ");
-        }
-    }
     int x = get_int("Number of newborn llamas: ");
     int y = get_int("Number of deceased llamas: ");
     int end = n + x - y;
