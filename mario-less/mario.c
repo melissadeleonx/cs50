f72e93f52f95a97813dda66253ae62d63acd40b6
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-09 23:47:02 +0000

    Sun, Sep 10, 2023, 1:47 AM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,47 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_height(void);
-void print_grid(int size);
-
+ // Goal is to 
 int main(void)
 {
-   // Get height of grid
-    int n = get_height();
-    // Print grid of bricks
-    print_grid(n);
-
-   // Add Iterate from 1 through height: On iteration i, print i hashes and then a newline
-}
 
 
+    //
 
-
-
-
-
-
-int get_height(void) // conditionals: if height < 1 or > 8 (or not a positive integer at all), prompt again and again (loop) until valid answer is received
-{
-    int n;
-    do
-    {
-        n = get_int("Height: ");
-    }
-    while (n < 1);
-    return n;
-    while (n > 8);
-    return n;
-}
-
-void print_grid(int size)
- {
-    for (int i = 0; i < size; i++)
- {
-    for (int j = 0; j < size; j++)
-    {
-        printf("#");
-        }
-        printf("\n");
-        }
 }
