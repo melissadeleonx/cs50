b86042d4d8046aa24306d7abd412e6cd950e47cf
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-10 16:19:56 +0000

    Sun, Sep 10, 2023, 6:19 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,32 +4,12 @@
 // Goal is to recreate a right-aligned pyramid
 
 int main(void) {
-    int height;
+    int height, rows, columns;
 
-    do {
-        // Prompt the user for the pyramid height with height more than 1 but not less than 8 and loop it
-        height = get_int("Enter the height of the pyramid (1-8): ");
-
-        if (height < 1 || height > 8) {
-            printf("Height should be between 1 and 8 inclusive.\n");
-        }
-    } while (height < 1 || height > 8);
-
-    // Use the 'for' function to make the row always subtracting 1 from the height
-    for (int i = 0; i < height; i++) {
-        // Print spaces
-        for (int j = 0; j < height - i - 1; j++) {
-            printf(" ");
-        }
-
-        // Print hashes
-        for (int j = 0; j < i + 1; j++) {
-            printf("#");
-        }
-
-        // Next line
-        printf("\n");
+    do { height = get_int("Height: ");
+    if(height < 1 || height > 8)
+    {
+        printf("Enter a height from 1 to 8");
     }
-
-    return 0;
+    } while(height < 1 || height > 8);
 }
