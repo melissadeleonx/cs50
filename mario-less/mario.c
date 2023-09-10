62ebdaea555106d7f7834b3387b0cdd5bb2a6627
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-10 16:26:23 +0000

    Sun, Sep 10, 2023, 6:26 PM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -9,9 +9,17 @@ int main(void) {
     do { height = get_int("Height: ");
     if(height < 1 || height > 8)
     {
-        printf("Enter a height from 1 to 8");
+        printf("Enter a number from 1 to 8\n");
     }
     } while(height < 1 || height > 8);
 
     for(rows = 0; rows < height; rows++)
+    {
+        for(columns = 0; columns < height; columns++)
+        {
+            printf("#");
+        }
+{ printf("\n");
+    }
+    return 0;
 }
