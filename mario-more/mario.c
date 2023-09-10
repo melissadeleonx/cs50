bb44569864621c8c7fff242d9fd2b247a1950c25
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-10 23:48:00 +0000

    Mon, Sep 11, 2023, 1:48 AM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -7,12 +7,26 @@ int main(void)
 {
     int height, rows, columns;
 
-    do { height = get_int("Height1: ");
+    do { height = get_int("Height: ");
     if(height < 1 || height > 8)
     {
         printf("Enter a number from 1 to 8\n");
     }
     } while(height < 1 || height > 8);
+
+    for(rows = 0; rows < height; rows++)
+    {
+        for(columns = 0; columns < (height - rows - 1); columns++)
+        {
+            printf(" ");
+        }
+        for(columns = 0; columns < (rows + 1); columns++)
+        {
+            printf("#");
+        }
+    printf("\n");
+    }
+}
 
     for(rows = 0; rows < height; rows++)
     {
