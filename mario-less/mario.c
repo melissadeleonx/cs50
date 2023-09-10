4f86125a00e355a533aecb8352611d99c5c1fa33
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-10 09:27:42 +0000

    Sun, Sep 10, 2023, 11:27 AM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -8,14 +8,16 @@ int main(void)
     // Prompt the user to put the height of the pyramid
 
     int height = get_int("Height: ");
+    int i = 1;
+    int j = 1;
 
     // Height always greater than 1
 
-    for (int i = 1; i <= height; i++);
+    for (i < height; i++);
 
     // Use the 'for' function to make the row always subtracting 1 from the height
         {
-            for int j = 1; j <= height - 1; j++;
+            for (j < height - 1; j++);
             {
             printf(" ");
         }
