30e52066008811b30d6caf1e58b7ce55401ee95f
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-10 14:56:01 +0000

    Sun, Sep 10, 2023, 4:56 PM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -15,34 +15,15 @@ int main(void)
         }
     } while (height < 1 || height > 8);
 
-       // 1st pyramid
-for (int i = 0; i < height; i++) {
+for (int i = height; i--) {
         // Print spaces
-        for (int j = 0; j < height - i - 1; j++) {
+        for (int j = 1; j < height - 1; j++) {
             printf(" ");
         }
-
         // Print hashes
-        for (int j = 0; j < i + 1; j++) {
+        for (int j = 1; j < i + 1; j++) {
             printf("#");
         }
-
-        // Next line
-        printf("\n");
-    }
-
-    // 2nd pyramid
-for (int i = height; i < height; i--) {
-        // Print spaces
-        for (int j = 0; j < height - i - 1; j--) {
-            printf(" ");
-        }
-
-        // Print hashes
-        for (int j = 0; j < i + 1; j++) {
-            printf("#");
-        }
-
         // Next line
         printf("\n");
     }
