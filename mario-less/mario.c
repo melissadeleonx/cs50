47506f8098206c95267b55aef946d234fc101ab2
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-10 09:00:23 +0000

    Sun, Sep 10, 2023, 11:00 AM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,11 +4,17 @@
  // Goal is to recreate a right-aligned pyramid of blocks Nintendo’s Super Mario Brothers
 int main(void)
 {
-    int n;
-    
     // Prompt the user to put the height of the pyramid, not less than 1 nor more than 8. Reprompt again if user gives an invalid answer.
+    int height;
+        height = get_int("Height: ")
 
-    // Use the 'for loop' to make the ascending #
+          // Use the 'for loop' to make the ascending with 
+        for i < 8; i > 1;
+        {
+            for j
+        }
+
+
 
     // Print # as bricks
 
