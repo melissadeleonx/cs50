33eedae020677391bc2823d718a3e1659c88fd06
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-10 13:02:15 +0000

    Sun, Sep 10, 2023, 3:02 PM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,10 +3,20 @@
 
 // Goal is to recreate Super Mario's two adjacent pyramids of blocks
 
-// 
+//
 
 int main(void)
 {
+    int height;
+
+    do {
+        // Prompt the user for the pyramid height with height more than 1 but not less than 8 and loop it
+        height = get_int("Enter the height of the pyramid (1-8): ");
+
+        if (height < 1 || height > 8) {
+            printf("Height should be between 1 and 8 inclusive.\n");
+        }
+    } while (height < 1 || height > 8);
 
 }
 
