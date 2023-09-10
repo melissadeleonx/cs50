acebebe7e19d7139fcde641392bbd3933adf1125
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-10 16:46:30 +0000

    Sun, Sep 10, 2023, 6:46 PM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -20,7 +20,7 @@ int main(void)
         {
          for(columns = 0; columns + height - 1; columns++)
          {
-            printf(" ")
+            printf(" ");
          }
         }
         {
@@ -28,23 +28,4 @@ int main(void)
         }
     printf("\n");
 }
-//2nd pyramid
-{
-    int height2, rows2, columns2;
-
-    do { height2 = get_int("Height2: ");
-    if(height2 < 1 || height > 8)
-    {
-        printf("Enter a number from 1 to 8\n");
-    }
-    } while(height2 < 1 || height2 > 8);
-
-    for(rows2 = 0; rows2 < height2; rows2++)
-    {
-        for(columns2 = 0; columns2 <= rows2; columns2++)
-        {
-            printf("#");
-        }
-    printf("\n");
-    }
 }
