51565c9a52959147c85473628035e2ac8290ad4d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-10 09:22:17 +0000

    Sun, Sep 10, 2023, 11:22 AM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,13 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
- // Goal is to recreate a right-aligned pyramid of blocks Nintendo’s Super Mario Brothers
+// Goal is to recreate a right-aligned pyramid
 
 int main(void)
 {
-    // Prompt the user to put the height of the pyramid, not less than 1 nor more than 8. Reprompt again if user gives an invalid answer.
+    // Prompt the user to put the height of the pyramid
+
     int height = get_int("Height: ");
 
+    //  not less than 1 nor more than 8. Reprompt again if user gives an invalid answer.
     // Use the 'for' function to make the ascending with the row always subtracting 1 from the height
 
     for (int i = 1; i <= height; i++);
@@ -16,9 +18,8 @@ int main(void)
             {
             printf(" ");
         }
-        }
     // Print # as bricks of the pyramid
-for (int j = 1; j <= i; j++) {
+    for (int j = 1; j <= i; j++) {
             printf("#");
         }
 
