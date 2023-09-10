046c9df8780e9069a299ed48e3557077a8c6f097
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-10 09:12:40 +0000

    Sun, Sep 10, 2023, 11:12 AM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -7,11 +7,11 @@ int main(void)
     // Prompt the user to put the height of the pyramid, not less than 1 nor more than 8. Reprompt again if user gives an invalid answer.
     int height = get_int("Height: ");
 
-    // Use the 'for' function to make the ascending with the row always less than 1 than the height
+    // Use the 'for' function to make the ascending with the row always subtracting 1 than the height
 
     for (int i = 1; i <= height; i++);
         {
-            for j = 1; j<=
+            for j = 1; j <= height - 1
         }
 
     // Print # as bricks of the pyramid
