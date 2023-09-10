8048425088a55f9bd3c27f9d061ea9a926c04b29
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-10 16:42:27 +0000

    Sun, Sep 10, 2023, 6:42 PM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -7,16 +7,19 @@ int main(void)
 {
     int height, rows, columns;
 
-    do { height = get_int("Height: ");
+    do { height = get_int("Height1: ");
     if(height < 1 || height > 8)
     {
         printf("Enter a number from 1 to 8\n");
     }
     } while(height < 1 || height > 8);
 
-    for(rows2 = 0; rows2 < height2; rows2++)
+    for(rows = 0; rows < height; rows++)
     {
-        for(columns2 = 0; columns2 <= rows2; columns2++)
+        for(columns = 0; columns <= rows; columns++)
+        {
+         for(columns)
+        }
         {
             printf("#");
         }
@@ -26,7 +29,7 @@ int main(void)
 {
     int height2, rows2, columns2;
 
-    do { height2 = get_int("Height: ");
+    do { height2 = get_int("Height2: ");
     if(height2 < 1 || height > 8)
     {
         printf("Enter a number from 1 to 8\n");
