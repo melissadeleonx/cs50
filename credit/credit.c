c80d29404b30193774a48bbad83c11e2f705356d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-11 20:05:25 +0000

    Mon, Sep 11, 2023, 10:05 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,15 +1,30 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_number(const char *prompt);
-int calculate_quarters(int cents);
-int calculate_dimes(int cents);
-int calculate_nickels(int cents);
-int calculate_pennies(int cents);
-
+long get_number(const char *prompt);
+int american_express(int number);
+int master_card(int number);
+int visa(int number);
 
 int main(void)
 {
     long number = get_number("Number: ")
 
+}
+
+
+long get_number(const char *prompt);
+{
+    long n;
+    do
+    {
+        n = get_long("Number: ");
+
+        if (n < 1)
+        {
+            printf("Please enter a positive number\n");
+        }
+    }
+    while (n < 1);
+    return n;
 }
