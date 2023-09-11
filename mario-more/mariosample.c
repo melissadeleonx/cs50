a4cd4d6b01d9a3802f6af1359cba5f8cc2c8a80f
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-11 00:28:43 +0000

    Mon, Sep 11, 2023, 2:28 AM +02:00

--- mario-more/mariosample.c
+++ mario-more/mariosample.c
@@ -15,7 +15,7 @@ int main(void)
     } while(height < 1 || height > 8);
 
     for(rows = 0; rows < height; rows++)
-    {for(rows = 0; rows < height; rows++)
+    { for(rows = 0; rows < height; rows++)
        { for(columns = 0; columns <= rows; columns++)
        { for(columns = 0; columns < (height - rows - 1); columns++)
         {
