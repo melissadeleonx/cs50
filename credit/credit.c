316817f9e323f2ea684c805f70c91dac93a3dcb2
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-11 20:01:45 +0000

    Mon, Sep 11, 2023, 10:01 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,8 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_number(const char *prompt);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
+
 int main(void)
 {
-    long number = get_long("Number: ")
-  
+    long number = get_number("Number: ")
+
 }
