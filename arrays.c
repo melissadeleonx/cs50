eac7f0d7e6162ef186517a6ba80b3b5071fa3aa6
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-14 16:31:53 +0000

    Thu, Sep 14, 2023, 6:31 PM +02:00

--- arrays.c
+++ arrays.c
@@ -6,7 +6,7 @@ int main (void)
 int score1 = get_int("Score1: ");
 int score2 = get_int("Score2: ");
 int score3 = get_int("Score3: ");
-printf("Average score is %i", (score1 + score2 + score3) / (float) 3);
+printf("Average score is %f\n", (score1 + score2 + score3) / (float)3);
 
 return 0;
 }
