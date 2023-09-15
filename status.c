3ad410c5379cfaf38ed2c30017e4a15cb6fe94c8
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-15 15:32:43 +0000

    Fri, Sep 15, 2023, 5:32 PM +02:00

---
+++ status.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    if(argc != 2)
+    {
+        printf("%s\n", argv[1]);
+    }
+    else {
+        printf("Missing info!!");
+    }
+
+}
