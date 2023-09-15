6d90f597ca0c46c7c5c19980117e7e26a63c6482
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-15 13:30:35 +0000

    Fri, Sep 15, 2023, 3:30 PM +02:00

--- hi.c
+++ hi.c
@@ -3,8 +3,10 @@
 
 int main(void)
 {
-   string s = "HI!";
-   string t = "BYE!";
+   string words[2];
 
-   printf("%i %i %i %i\n", t[0], t[1], t[2], t[3]);
+   words[0] = "HI!";
+   words[1] = "BYE!";
+
+   printf("%c%c%c\n%c%c%c%c", words[0], words[1]);
 }
