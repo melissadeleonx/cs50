b20600fcce3181028ae935f5eef6920cc08da134
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-15 14:10:53 +0000

    Fri, Sep 15, 2023, 4:10 PM +02:00

--- uppercase.c
+++ uppercase.c
@@ -11,7 +11,7 @@ int main(void)
     {
         if(s[i] >= 'a' && s[i] < 'z')
         {
-            printf("%c", s[i] - 32);
+        printf("%c", toupper (s[i]=));
         }
         else
         {
