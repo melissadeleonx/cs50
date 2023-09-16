8b90247f70b228030b55499622a27d2c6830928f
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-16 15:56:38 +0000

    Sat, Sep 16, 2023, 5:56 PM +02:00

--- hours/hoursEx.c
+++ hours/hoursEx.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 // Mention new function on the top
-float calc_hours(int hours[], int weeks, char output);
+float calc_hours(int hours[], int weeks);
 
 int main(void)
-{
+{   // Given codes by cs50
     int weeks = get_int("Number of weeks taking CS50: ");
     int hours[weeks];
 
@@ -17,7 +17,7 @@ int main(void)
 
     char output;
     do
-    {
+    {   // Make the conditions 'T' for total hours and 'A' for average hours
         output = toupper(get_char("Enter T for total hours, A for average hours per week: "));
 
         if(output == 'T')
@@ -31,17 +31,15 @@ int main(void)
         } else if(output == 'A')
         {
             float Average = calc_hours(hours, weeks);
-            printf("The average is %.2f", Average);
+            printf("The average is %.2f\n", Average);
         }
           }
     while (output != 'T' && output != 'A');
 }
 
 
-
-
 // TODO: complete the calc_hours function
-float calc_hours(int hours[], int weeks, char output)
+float calc_hours(int hours[], int weeks)
 {
         int Totalhours = 0;
         for (int i = 0; i < weeks; i++)
