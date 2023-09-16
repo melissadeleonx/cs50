767594c3e1d45e84f110bbc1cf7587e1a5a96399
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-16 16:00:40 +0000

    Sat, Sep 16, 2023, 6:00 PM +02:00

--- hours/hours.c
+++ hours/hours.c
@@ -2,10 +2,11 @@
 #include <ctype.h>
 #include <stdio.h>
 
-float calc_hours(int hours[], int weeks, char output);
+// Mention new function on the top
+float calc_hours(int hours[], int weeks);
 
 int main(void)
-{
+{   // Given codes by cs50
     int weeks = get_int("Number of weeks taking CS50: ");
     int hours[weeks];
 
@@ -16,13 +17,34 @@ int main(void)
 
     char output;
     do
-    {
+    {   // Make the conditions 'T' for total hours and 'A' for average hours
         output = toupper(get_char("Enter T for total hours, A for average hours per week: "));
-    }
-    while (output != 'T' && output != 'A');
 
-    printf("%.1f hours\n", calc_hours(hours, weeks, output));
+        if(output == 'T')
+        {
+            int Totalhours = 0;
+            for (int i = 0; i < weeks; i++)
+            {
+                Totalhours += hours[i];
+                }
+            printf("The Total is %i hours\n", Totalhours);
+        } else if(output == 'A')
+        {
+            float Average = calc_hours(hours, weeks);
+            printf("The average is %.2f\n", Average);
+        }
+          }
+    while (output != 'T' && output != 'A');
 }
 
+
 // TODO: complete the calc_hours function
-float calc_hours(int hours[], int weeks, char output) {}
+float calc_hours(int hours[], int weeks)
+{
+        int Totalhours = 0;
+        for (int i = 0; i < weeks; i++)
+    {
+        Totalhours += hours[i];
+    }
+        return (float)Totalhours / weeks;
+}
