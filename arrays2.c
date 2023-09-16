f53cc90fd8bc10ea5e4e15025d2a5f42732dc185
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-16 05:49:44 +0000

    Sat, Sep 16, 2023, 7:49 AM +02:00

--- arrays2.c
+++ arrays2.c
@@ -1,13 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
+const int N = 5;
 int main (void)
 {
-    int size[];
+    int size[N];
 
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < N; i++)
     {
         size[i] = get_int("size: ");
         }
-        printf("%i\n", n[]);
+        printf("%i\n", size[N]);
 }
