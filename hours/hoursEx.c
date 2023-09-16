61b2bae1300d443c322f9ebc9ece9cf39ccf9741
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-16 15:27:35 +0000

    Sat, Sep 16, 2023, 5:27 PM +02:00

--- hours/hoursEx.c
+++ hours/hoursEx.c
@@ -35,9 +35,5 @@ int main(void)
 // TODO: complete the calc_hours function
 float calc_hours(int hours[], int weeks, char output)
 {
-    for(int i = 0; i < weeks; i++)
-    {
         return hours[i] / weeks;
-    }
-
 }
