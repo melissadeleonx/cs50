0a7b84d30c6239dcbab923066a9867c0e26e825d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-16 09:35:14 +0000

    Sat, Sep 16, 2023, 11:35 AM +02:00

--- week2/alpha.c
+++ week2/alpha.c
@@ -1,12 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main (void)
 { string word;
     // Prompt the user to put lowercase letters from a-f)
         word = get_string("Please type from a-f: ");
-        if(){
+        if(islower ){
             printf("Yes");
         }
 
