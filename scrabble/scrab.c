068e1b5e8d18a007ab39d99d5ce33e04331291ff
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-16 22:02:44 +0000

    Sun, Sep 17, 2023, 12:02 AM +02:00

--- scrabble/scrab.c
+++ scrabble/scrab.c
@@ -25,5 +25,7 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    int compute_score = return word[i];
+    string word = 0;
+    
+    return word = POINTS[i]
 }
