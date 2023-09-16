41155064c5069fc8a6ebeff14cdce3885f9328db
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-16 08:12:39 +0000

    Sat, Sep 16, 2023, 10:12 AM +02:00

--- arrays2.c
+++ arrays2.c
@@ -7,14 +7,14 @@ int main (void)
     int length;
     do
     {
-        length = get_int("Lenght: ");
+        length = get_int("Length: ");
     }
     while(length < 0);
 
     int twice[length];
     for(int i = 0; i < length; i++)
    {
-    twice[i] = (twice[i] - 1) * 2;
-
-    printf("%i\n", twice);}
+    twice[i] = twice[i-1] * 2;
+    printf("%i\n", twice[i]);
+    }
    }
