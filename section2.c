4349c5e13341816dfb92bceac1e6ad025ff6fa07
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-16 05:14:55 +0000

    Sat, Sep 16, 2023, 7:14 AM +02:00

--- section2.c
+++ section2.c
@@ -3,10 +3,11 @@
 
 int main (void)
 {
-    int hours[] = {7,9, 8, 7, 8};
+    int hours[5] = {7, 9, 8, 7, 8};
+
     for(int i = 0; i < 5; i++)
     {
-        printf("%i\n");
+        printf("%i\n", hours[i]);
     }
 return 0;
 }
