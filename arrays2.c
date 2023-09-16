de1845a920f292d78c72686d2c7493e337789f3c
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-16 05:30:36 +0000

    Sat, Sep 16, 2023, 7:30 AM +02:00

--- arrays2.c
+++ arrays2.c
@@ -3,12 +3,15 @@
 
 int main (void)
 {
-    int size[];
+    int size[5];
     do
     {
         size = get_int("Size: ");
     }
     while (size < 0);
 
-
+    for(int i = 1; i < 5; i++)
+    {
+        printf("%i\n", int size[i]);
+    }
 }
