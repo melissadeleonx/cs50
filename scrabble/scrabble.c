9798caa3f107501cc824f9adebd880ab83885e04
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-17 08:03:54 +0000

    Sun, Sep 17, 2023, 10:03 AM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -7,12 +7,13 @@
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 // Mention new function on the top
+const int N = 2;
 int compute_score(string word, int word_length);
 
 int main(void)
 {
     // Get input words from both players
-    string word[];
+    string word[N];
     word[0] = get_string("Player 1: ");
     word[1] = get_string("Player 2: ");
 
@@ -29,7 +30,7 @@ int main(void)
     }
 }
 
-int compute_score(string word, int word_length)
+int compute_score(string word[N], int word_length)
 {
     // TODO: Compute and return score for string
         int sum = 0;
