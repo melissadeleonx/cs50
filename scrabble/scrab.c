e0f4d84672ddc127f20990f886f8400ed0953b1c
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-17 06:17:27 +0000

    Sun, Sep 17, 2023, 8:17 AM +02:00

--- scrabble/scrab.c
+++ scrabble/scrab.c
@@ -17,7 +17,7 @@ int main(void)
     int word_length = strlen(word1);
 
     // Score both words
-    int score1 = compute_score;
+    int score1 = compute_score(word1);
 
     printf("The score is %i\n", score1);
 
