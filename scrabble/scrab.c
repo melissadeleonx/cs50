1cb27f113636520610ed28d3abc5ad1ee0519416
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-17 08:11:07 +0000

    Sun, Sep 17, 2023, 10:11 AM +02:00

--- scrabble/scrab.c
+++ scrabble/scrab.c
@@ -7,22 +7,21 @@
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 // Mention new function on the top
-int compute_score(string word1, int word_length);
+const int N = 2;
+int compute_score(string word[N], int word_length);
 
 int main(void)
 {
-    string word = get_string("Word: ");
-    int word_length = strlen(word);
-    int score = compute_score(word);
-    printf("%i", score)
-}
-
-
-int compute_score(string word, int word_length)
-{
-    int sum = 0;
-    for(int i = 0; i < word_length - 1; i++)
+    // Get input words from both players
+    string word[N];
+    for(int i = 0; i < N; i++)
     {
-        sum += POINTS[i];
-    }  printf("The score is %i\n", sum);
+        word[0] = get_string("Player 1: ");
+        word[1] = get_string("Player 2: ");
+        }
+    // Score both words
+
+    // TODO: Print the winner
+
 }
+
--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -8,14 +8,18 @@ int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 
 
 // Mention new function on the top
 const int N = 2;
-int compute_score(string word, int word_length);
+int compute_score(string word[N], int word_length);
 
 int main(void)
 {
     // Get input words from both players
     string word[N];
-    word[0] = get_string("Player 1: ");
-    word[1] = get_string("Player 2: ");
+    for(int i = 0; i < N; i++)
+    {
+        word[0] = get_string("Player 1: ");
+        word[1] = get_string("Player 2: ");
+        }
+
 
     // Score both words
     int score1 = compute_score(word[0]);
