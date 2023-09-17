313bba2744995208a8f7109cf7183bb40bb35583
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-17 09:13:14 +0000

    Sun, Sep 17, 2023, 11:13 AM +02:00

--- scrabble/scrab.c
+++ scrabble/scrab.c
@@ -8,7 +8,7 @@ int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 
 
 // Mention new function on the top
 const int N = 2;
-int compute_score(string word[N], int word_length);
+int compute_score(string word);
 
 int main(void)
 {
