a1ca2db24c945ee3be4ac0c36a20f3feb99cc5ba
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-17 15:36:56 +0000

    Sun, Sep 17, 2023, 5:36 PM +02:00

---
+++ readability/read.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+
+// Readability Test
+// New functions to calculate number of letters in the text
+int count_letters(string text)
+int count_words(string text)
+int count_sentences(string text)
+
+// Coleman-Liau index computation is index = 0.0588 * L - 0.296 * S - 15.8
+
+int main(void)
+{
+    //
+
+}
+
+// Grade Classifications
+ (Before Grade 1)
+ (Grade 2)
+  (Grade 3)
