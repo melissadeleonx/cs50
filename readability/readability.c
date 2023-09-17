8f34515848b6de07439f1d9962fff58abf61dfa2
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-17 13:58:38 +0000

    Sun, Sep 17, 2023, 3:58 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,11 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// Readability Test 
+// Readability Test
+
+// New functions to calculate number of letters in the text
 int count_letters(string text)
 
 int main(void)
 {
-
+    //
 
 }
