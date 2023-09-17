a2a95c5e1423907f8195900e842e554ead754f45
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-17 06:03:17 +0000

    Sun, Sep 17, 2023, 8:03 AM +02:00

--- scrabble/scrab.c
+++ scrabble/scrab.c
@@ -6,6 +6,9 @@
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
+// use strlen?
+int length = strlen 
+
 // Mention new function on the top
 int compute_score(string word);
 
