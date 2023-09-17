a6db2b719ddfdfd35aa5004d5f5dfef5e1677991
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-17 14:04:33 +0000

    Sun, Sep 17, 2023, 4:04 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,7 +8,8 @@ int count_letters(string text)
 int count_words(string text)
 int count_sentences(string text)
 
-// Coleman-Liau index computation is 
+// Coleman-Liau index computation is index = 0.0588 * L - 0.296 * S - 15.8
+
 int main(void)
 {
     //
