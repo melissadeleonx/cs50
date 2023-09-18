b612f5051d0845ae3989f8a6c778e89d97aa520e
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-18 11:16:54 +0000

    Mon, Sep 18, 2023, 1:16 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,12 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <math.h>
 
 // Readability Test
 // New functions to calculate number of letters in the text
-int count_letters(string text)
-int count_words(string text)
-int count_sentences(string text)
+string text(const char *prompt);
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
 
 // Coleman-Liau index computation is index = 0.0588 * L - 0.296 * S - 15.8
 
