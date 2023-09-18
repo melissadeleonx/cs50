6f999416fd4df36197a73239d34383d293cb0162
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-18 11:47:23 +0000

    Mon, Sep 18, 2023, 1:47 PM +02:00

--- readability/read.c
+++ readability/read.c
@@ -1,15 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <ctype.h>
-
-// Readability Test
-// New functions to calculate number of letters in the text
-// Coleman-Liau index computation is index = 0.0588 * L - 0.296 * S - 15.8
+#include <string.h>
 
 int main(void)
 {
-    // Start the readability program
-    string text = get_string("Text: ");
-    int n = strlen(text);
-    printf("%i\n", n);
+    string s = get_string("Input: ");
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c", s[i]);
     }
+    printf("\n");
+}
