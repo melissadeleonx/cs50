f8590908d6b526b7b881a34fb32e5c07cd2322c9
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-18 11:22:39 +0000

    Mon, Sep 18, 2023, 1:22 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 
 // Readability Test
-// New functions to calculate number of letters in the text
+// New functions to calculate number of letters, words and sentences in the text
 string text(const char *prompt);
 int count_letters(string text);
 int count_words(string text);
@@ -17,8 +17,30 @@ int main(void)
 
 }
 
-
+// Function to count the number of letters
 int count_letters(string text)
+{
+    int letters = 0;
+    for (int i = 0; text[i]; i++) {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    }
+    return letters;
+}
 
+// Function to count the number of words
+
+{
+    int words = 0;
+    for (int i = 0; text[i]; i++) {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    }
+    return letters;
+}
 
 
