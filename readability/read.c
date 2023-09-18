1f15ad01f73817202b7f4c924c4873d9e4aa02a7
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-18 13:59:23 +0000

    Mon, Sep 18, 2023, 3:59 PM +02:00

--- readability/read.c
+++ readability/read.c
@@ -1,19 +1,91 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+
+// Readability Test
+// New functions to calculate number of letters, words and sentences in the text
+string text(const char *prompt);
+int count_letters(string text); //done
+int count_words(string text);   //done
+int count_sentences(string text); //done
 
 int main(void)
 {
-   string text = get_string("Text: ");
-      int sentences = 0;
+    string text = get_string("Text: ");
+    float clindex(string text);
+    string grade_level = grade_level(clindex);
+
+
+//Coleman-Liau index computation is 0.0588 * L - 0.296 * S - 15.8
+// Function for Coleman-Liau index, result, and grade level classification
+
+clindex(string text)
+{
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    // Use the formula with average number of letters per 100 words(L) and the average number of sentences per 100 words(S)
+    float L = (float)letters / words * 100;
+    float S = (float)sentences / words * 100;
+
+    clindex = round (0.0588 * L - 0.296 * S - 15.8);
+
+        // Grade Level Classification - determine the readability level of the user
+    if(clindex > 1)
+    {
+        printf("Before Grade 1");
+    } else if(clindex < 16)
+    {
+        printf("Grade 16+");
+    } else
+    {
+        printf("Grade %i", clindex);
+    }
+}
+}
+
+
+
+
+
+// Function to count the number of letters
+int count_letters(string text)
+ {
+    int letters = 0;
+    for(int i = 0; text[i] != '\0'; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    } return 0;
+}
+
+// Function to count the number of words
+
+int count_words(string text)
+{
+    int words = 0;
+    for(int i = 0; text[i] != '\0'; i++)
+    {
+        if(isspace(text[i]))
+        {
+            words++;
+        }
+    } return 0;
+}
+
+// Function to count the number of words
+int count_sentences(string text)
+{
+    int sentences = 0;
     for (int i = 0; text[i] != '\0'; i++)
     {
         if(ispunct(text[i]))
         {
             sentences++;
         }
-    } printf("Total sentences is %i\n", sentences);
-    return 0;
+    } return 0;
 }
 
