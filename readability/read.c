7269609cfeea1da974353c7bf8280fd662bc0179
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-18 03:43:26 +0000

    Mon, Sep 18, 2023, 5:43 AM +02:00

--- readability/read.c
+++ readability/read.c
@@ -4,7 +4,6 @@
 
 // Readability Test
 // New functions to calculate number of letters in the text
-int count_letters(string text);
 
 // Coleman-Liau index computation is index = 0.0588 * L - 0.296 * S - 15.8
 
@@ -12,20 +11,8 @@ int main(void)
 {
     // Start the readability program
     string text = get_string("Text: ");
+    int count_letters = long strlen(text);
     printf("Text Number is %i\n", count_letters);
 
     return 0;
-}
-
-int count_letters(string text);
-{
-    int letters = 0;
-    for(int i = 0; i < text[i]; i++)
-    {
-         if (isalpha(text[i]))
-         {
-            letters++;
-         }
-
-    }
 }
