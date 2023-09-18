11b0e9b71e06b4bbd39d4d452d899dd99ae6782b
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-18 03:36:10 +0000

    Mon, Sep 18, 2023, 5:36 AM +02:00

--- readability/read.c
+++ readability/read.c
@@ -4,9 +4,7 @@
 
 // Readability Test
 // New functions to calculate number of letters in the text
-int count_letters(string text)
-int count_words(string text)
-int count_sentences(string text)
+int count_letters(string text);
 
 // Coleman-Liau index computation is index = 0.0588 * L - 0.296 * S - 15.8
 
@@ -14,22 +12,20 @@ int main(void)
 {
     // Start the readability program
     string text = get_string("Text: ");
-    float cli_score = calculate_cli(text);
-    string readability = interpret_cli_score(cli_score);
-
-    printf("CLI Score: %.2f\n", cli_score);
-    printf("Readability: %s\n", readability);
+    printf("Text Number is %i\n", count_letters);
 
     return 0;
 }
 
-// Grade Classifications
- (Before Grade 1)
- (Grade 2)
-  (Grade 3)
-  (Grade 5)
-  (Grade 7)
-  (Grade 8)
-  (Grade 9)
-  (Grade 10)
-  (Grade 16+)
+int count_letters(string text);
+{
+    int letters = 0;
+    for(int i = 0; i < text[i]; i++)
+    {
+         if (isalpha(text[i]))
+         {
+            letters++;
+         }
+
+    }
+}
