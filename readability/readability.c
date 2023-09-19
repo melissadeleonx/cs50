cae208daa252154513ed855e837e2d07fa1d2838
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-19 09:25:23 +0000

    Tue, Sep 19, 2023, 11:25 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,8 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <ctype.h>
-#include <string.h>
 #include <math.h>
+#include <stdio.h>
+#include <string.h>
 
 // Week 2 Problem Set: Readability Test
 // New functions to calculate number of letters, words and sentences of the text
@@ -18,12 +18,12 @@ int main(void)
     string text = get_string("Write a text: ");
     float clindex_grade = calculate_clindex(text);
 
-        // Grade Level Classification - determine the readability level of the user
-    if(clindex_grade > 1)
+    // Grade Level Classification - determine the readability level of the user
+    if (clindex_grade > 1)
     {
         printf("Before Grade 1\n");
     }
-    else if(clindex_grade >= 16)
+    else if (clindex_grade >= 16)
     {
         printf("Grade 16+\n");
     }
@@ -34,37 +34,32 @@ int main(void)
     return 0;
 }
 
-
-
-
-
-
-
-
 // Function to count the number of letters
 int count_letters(string text)
- {
+{
     int letters = 0;
-    for(int i = 0; text[i] != '\0'; i++)
+    for (int i = 0; text[i] != '\0'; i++)
     {
         if (isalpha(text[i]))
         {
             letters++;
         }
-    } return letters;
+    }
+    return letters;
 }
 
 // Function to count the number of words
 int count_words(string text)
 {
     int words = 1;
-    for(int i = 0; text[i] != '\0'; i++)
+    for (int i = 0; text[i] != '\0'; i++)
     {
-        if(isspace(text[i]))
+        if (isspace(text[i]))
         {
             words++;
         }
-    } return words;
+    }
+    return words;
 }
 
 // Function to count the number of words
@@ -73,11 +68,12 @@ int count_sentences(string text)
     int sentences = 0;
     for (int i = 0; text[i] != '\0'; i++)
     {
-        if(text[i] == '.' || text[i] == '!' || text[i] == '?')
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
         {
             sentences++;
         }
-    } return sentences;
+    }
+    return sentences;
 }
 
 // Function to calculate the Coleman-Liau index and classify readability grade level
@@ -88,10 +84,10 @@ float calculate_clindex(string text)
     int sentences = count_sentences(text);
 
     // Use the formula with average number of letters per 100 words(L) and the average number of sentences per 100 words(S)
-    float L = (float)letters / words * 100;
-    float S = (float)sentences / words * 100;
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
 
     // Coleman-Liau index formula
     float clindex_grade = 0.0588 * L - 0.296 * S - 15.8;
     return round(clindex_grade);
- }
+}
