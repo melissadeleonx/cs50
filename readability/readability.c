e2790e690f7537f9760262bc560440b6c9f5b055
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-19 08:39:29 +0000

    Tue, Sep 19, 2023, 10:39 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -56,7 +56,7 @@ int count_letters(string text)
 // Function to count the number of words
 int count_words(string text)
 {
-    int words = 1;
+    int words = 0;
     for(int i = 0; text[i] != '\0'; i++)
     {
         if(isspace(text[i]))
