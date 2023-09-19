3006df57860f84e29068b6d52285a600f1aedfaa
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-19 19:11:56 +0000

    Tue, Sep 19, 2023, 9:11 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -58,8 +58,9 @@ int count_words(string text)
     int words = 1;
     for (int i = 0; i < strlen(text); i++)
     {
-        if ((text[i]))
+        if (isalpha(text[i]))
         {
+            if(text[i] == ' ')
             words++;
         }
     } return words;
