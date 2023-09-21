749e7e2679cf8512ad60c07a62fd608651053123
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-21 21:10:58 +0000

    Thu, Sep 21, 2023, 11:10 PM +02:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -11,7 +11,7 @@ int main(void)
     string text = get_string("Enter your message here:  ");
     for (int i = 0; i < strlen(text); i++)
     {
-        print_bulb("%i\n", text[i]);
+        printf("%i\n", text[i]);
     }
 
 }
