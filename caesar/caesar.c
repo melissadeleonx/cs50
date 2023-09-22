fd83b96cc4df8552f3b0f9cd9f5068d675f4685f
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-22 22:07:13 +0000

    Sat, Sep 23, 2023, 12:07 AM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,23 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Design and implement a program, caesar, that encrypts messages using Caesar’s cipher.
 // Make sure program was run with just one command-line argument
 int main(int argc, string argv[])
 {
-  int main(void)
-{
 
 
-    // Make sure every character in argv[1] is a digit
+    // Make sure every character in argv[1] is a digit, a non-negative integer.
 
     // Convert argv[1] from a `string` to an `int`
 
     // Prompt user for plaintext
+
 
     // For each character in the plaintext:
 
         // Rotate the character if it's a letter
 }
 
-
-}
