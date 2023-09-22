08a4d5665e68f96183b11d7ff2cbf2a82863cbc8
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-22 21:59:05 +0000

    Fri, Sep 22, 2023, 11:59 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,11 +4,11 @@
 int main(int argc, string argv[])
 {
     string plaintext = get_string("plaintext: ");
-    for (i = argv[1]; i < strlen(plaintext); i++)
+    for (int i = argv[1]; i < strlen(plaintext); i++)
     {
-        
+         printf("ciphertext: %s\n", plaintext);
     }
 
-    printf("ciphertext: %s\n", plaintext);
+
 
 }
