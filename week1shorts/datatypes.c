66bb48eb4f3638fce1ae82c532d023729c2a2d04
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-25 08:23:52 +0000

    Mon, Sep 25, 2023, 10:23 AM +02:00

--- week1shorts/datatypes.c
+++ week1shorts/datatypes.c
@@ -9,7 +9,8 @@ int main(void)
     printf("Great, Data types and variables are kinda unique to C\n");
     string answer2 = get_string("Do you want to know why? ");
     {
-        printf("If you've been a programmer for a really long time, you've probably not worked with data types
-    if you've used modern programming languages");
+        printf("you've been a programmer for a really long time\n";
+        "you've probably not worked with data types if you've used\n";
+         "modern programming languages");
     }
 }
