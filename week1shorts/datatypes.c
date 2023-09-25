f21a921adf11b5d77af36c6c0d7382a739b4988c
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-25 08:02:10 +0000

    Mon, Sep 25, 2023, 10:02 AM +02:00

---
+++ week1shorts/datatypes.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+
+{
+    int integer = 0;
+    printf("Integers or int %i\n", integer);
+    printf("Unsigned Integer, if the value will never be negative\n");
+
+}
