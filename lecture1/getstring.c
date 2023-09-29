e29d5ba4d7873217f6161d04cd9c52f3d8813905
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-29 21:32:24 +0000

    Fri, Sep 29, 2023, 11:32 PM +02:00

--- lecture1/getstring.c
+++ lecture1/getstring.c
@@ -13,6 +13,9 @@ int main(void)
     // \n is for the next line or create a new line - please note that compiler tools are not user-friendly, you need to work around it to avoid errors!!
     // what is the % for? it is syntax(format code) to identify the data types, an identifier
     // stdio.h and cs50.h libraries(extensions) have the functions, any functions, one need to include its library
+    // the cs50.h library is automatically done through cloud for the cs50 students
     // get_string is for Cs50 but there are equivalent functions from other libraries
-    //
+    // text editor can change colors but it does not really affect the code, it is for the appearance and for focus and mistake, visually useful, to check if the (){}are aligned to each other
+    // %i %s %d %.f
+
 }
