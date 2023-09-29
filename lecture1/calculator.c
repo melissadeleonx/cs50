62b2afeb616993e53d0197454e9c59d6a75b060f
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-29 22:29:25 +0000

    Sat, Sep 30, 2023, 12:29 AM +02:00

--- lecture1/calculator.c
+++ lecture1/calculator.c
@@ -12,6 +12,6 @@ int main(void)
     printf("%i\n", first_number + second_number);
 // Simpler, shorter codes - the code depends on the intention of the program
 // From x and y, we can name our variables depending on the context of the program
-// Integer like
+// Integer likecd 
 
 }
