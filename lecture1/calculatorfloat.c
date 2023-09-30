a2bb3c80aba2f8c2c9f7b3b60214fa414a1b5f16
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 21:32:35 +0000

    Sat, Sep 30, 2023, 11:32 PM +02:00

--- lecture1/calculatorfloat.c
+++ lecture1/calculatorfloat.c
@@ -5,6 +5,7 @@
 // Floating-point imprecision - inability of the computer to represent all possible real numbers - In C, this problem occurs while other scientific languages already found a solution
 // Integers are allowed to have 32 bits(2 billions approximately for positive and another 2 billion for negative) by standard and floating-point imprecision occur when all of these bits are use and anything excessive is converted to 0000
 // Another example of integer overflow is the year 2000 since computer then only count by 2, 95 96 97 98 99, and 00 is like coming back to 1900, the same will happen on January 19, 2038 with regards to the number of seconds that will be exhausted by computers
+// Thankfully, it is no longer a problem as computer nowadays have 64 bits, doubling the integer space available
 
 // First version of the code, we use float discussing the above topics
 // Second version, we are trying with int data type of x and y with float z. The result of the division is 0.0000 which is called truncation in C(throwing away the decimals)
