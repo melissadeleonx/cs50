1b78bb69bddf8d2644cd9f5a2b88a5a7087a68c5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 15:29:41 +0000

    Sat, Sep 30, 2023, 5:29 PM +02:00

--- lecture1/marioright.c
+++ lecture1/marioright.c
@@ -12,11 +12,11 @@ int main(void)
     } while (n < 1);
 
     //
-    for (int i = 0; i < n - i; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < n - i; j++)
         {
-            printf("#");
+            printf(" ");
         }
         printf("\n");
     }
