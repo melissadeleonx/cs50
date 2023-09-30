f3bcb41f5b63b2b6805556ec47bb07670952694c
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 08:29:59 +0000

    Sat, Sep 30, 2023, 10:29 AM +02:00

--- lecture1/agree.c
+++ lecture1/agree.c
@@ -3,17 +3,19 @@
 
 int main(void)
 {
+    // Prompt user to agree, if only a single letter, use char or char datatype
     char answer = get_char("Do you agree? ");
+    // Check whether user agreed
     if (answer == 'y' || answer == 'Y')
     {
         printf("Agreed\n");
     }
     else if (answer == 'n' || answer == 'N')
     {
-        printf("Not agreed");
+        printf("Not agreed\n");
     }
     else
     {
-        printf("Please answer yes or no");
+        printf("Please answer Y for yes or N for no.\n");
     }
 }
