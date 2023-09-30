b5201d6aafc6449949ece46daa23e4c34252f34a
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 12:54:44 +0000

    Sat, Sep 30, 2023, 2:54 PM +02:00

--- lecture1/discountfunc.c
+++ lecture1/discountfunc.c
@@ -3,17 +3,27 @@
 
 float discount(float regular);
 // A modified version of discount.c with our created function called float discount
-// Sometimes, the simpler codes are the better codes
+// Sometimes, the SIMPLER CODES are the BETTER CODES
+// Watch out for the data types inside the () of the new functions, the return of the input, for this case, the prompted regular from the user
+// Do not complicate life!
 
 int main(void)
 {
     float regular = get_float("Regular Price: ");
-    printf("The Sale Price is %.2f", discount);
+    float sale = discount(regular);
+    printf("The Sale Price is %.2f\n", sale);
 
 }
 
 
-float discount(int regular)
+
+
+
+
+
+
+
+float discount(float regular)
 {
-    return * .85;
+    return regular * .85;
 }
