0e9aa25fd36345c0d277986838ae7bd4e38307fb
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 12:22:45 +0000

    Sat, Sep 30, 2023, 2:22 PM +02:00

--- lecture1/function.c
+++ lecture1/function.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-void meow(void);
+void meow(int n); // You can put void on the function so it won't have any input or you can put something inside the () which can customize its definition
+
 
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
-    {
-        meow();
+    // Since we function meow is no longer define as void, we can remove the LOOP for which makes it meow a few times
+    // I think we need to define the int n???
+        {
+            int n = 3;
+            meow(3);
     }
 }
 
@@ -19,7 +22,12 @@ int main(void)
 
 
 // As long as you declare the function at the top before the main, the program will identity the functions, it reads from top to bottom
-void meow(void)
+// Define the function like below
+// Other languages allow functions in any order but not in C, it is cooler more complicated language so FOLLOW THE ORDER!!!
+void meow(int n)
 {
-    printf("meow\n");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("meow\n");
+    }
 }
