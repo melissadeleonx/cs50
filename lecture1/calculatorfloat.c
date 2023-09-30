9174bde7637379e0f1bd4dedda983f78652491aa
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 21:10:19 +0000

    Sat, Sep 30, 2023, 11:10 PM +02:00

--- lecture1/calculatorfloat.c
+++ lecture1/calculatorfloat.c
@@ -3,16 +3,17 @@
 
 // Explain the issue with integer overflow, using a lot of bits but there are not much availability. Computer is only providing the closest approximation.
 // Floating-point imprecision - inability of the computer to represent all possible real numbers - In C, this problem occurs while other scientific languages already found a solution
-
+// First version of the code, we use float discussing the above topics
+// Second version, we are trying with int data type
 int main(void)
 {
 // Prompt user for x
-    float x = get_float("x: ");
+    int x = get_int("x: ");
 
     // Promp user for y
-    float y = get_float("y: ");
+    int y = get_int("y: ");
 
     // Divide x by y
     float z = x / y;
-    printf("%.50f is the quotient of x by y\n", z);
+    printf("%.f is the quotient of x by y\n", z);
 }
