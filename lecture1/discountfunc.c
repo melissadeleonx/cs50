704546fa7c0d26d5a7ba09885a52a671f963addb
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 12:56:49 +0000

    Sat, Sep 30, 2023, 2:56 PM +02:00

--- lecture1/discountfunc.c
+++ lecture1/discountfunc.c
@@ -11,7 +11,7 @@ int main(void)
 {
     float regular = get_float("Regular Price: ");
     float sale = discount(regular);
-    printf("The Sale Price is %.2f\n", sale);
+    printf("The Sale Price is $%.2f\n", sale);
 
 }
 
@@ -22,7 +22,7 @@ int main(void)
 
 
 
-
+// The new functions is not printing anything but handling a value to a variable to make the code cleaner
 float discount(float regular)
 {
     return regular * .85;
