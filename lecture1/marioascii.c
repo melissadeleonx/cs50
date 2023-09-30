44250b7b8155738ff1cecbadd42699b0288dd1b0
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 15:03:35 +0000

    Sat, Sep 30, 2023, 5:03 PM +02:00

--- lecture1/marioascii.c
+++ lecture1/marioascii.c
@@ -11,9 +11,9 @@ int main(void)
         n = get_int("Size: "); // Size in this context is the height and widht of the left pyramid
     } while (n < 1);
 
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++) // (--) always lead me to error, lol!
+        for (int j = 0; j < i + 1; j++) // (--) always lead me to error, lol!
         {
             printf("#");
         }
