48cfc711d261c69e275b41ed18e36bbf07c4b82a
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 07:49:59 +0000

    Sat, Sep 30, 2023, 9:49 AM +02:00

--- lecture1/points.c
+++ lecture1/points.c
@@ -4,7 +4,8 @@
 int main(void)
 {
     const int MINE = 2; // Initializing a variable to use for conditionals so you can change it more easily and design is better
-    // Make the name capitalize as good practice for constant variables, as a reminder that somewhere somehow it is a constant
+    // Make the name capitalize as good practice for constant variables, as a reminder that somewhere somehow it is a constant. Instilling a heuristic - mental shortcut
+    
     int point = get_int("How many points did you lose? ");
     if (point < MINE)
     {
