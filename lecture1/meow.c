236e1e5fbd67e979a3e3284e8d077d6d05450b55
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 09:26:52 +0000

    Sat, Sep 30, 2023, 11:26 AM +02:00

--- lecture1/meow.c
+++ lecture1/meow.c
@@ -5,11 +5,11 @@
 
 int main(void)
 {
-    // while loop also take bool function, one case use 1 for true and 0 for false or just as it is to be explicit
-    int i = 0; // counter is what used in Scratch
-    while (i < 3)
+    // while loop also take bool function(condition(<=,>=,==, operators)), one case use 1 for true and 0 for false or just as it is to be explicit
+    int i = 2; // counter is what used in Scratch
+    while (i <= 10)
     {
         printf("meow\n");
-        i++;
+        i++; // to increment from 0 and up, syntatic sugar if you want to implement how many times you want to repeat depending on the boolean function inside while
     }
 }
