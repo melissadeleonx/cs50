dceba1ac588cd913571567154599b7c03b616ac5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 08:09:59 +0000

    Sat, Sep 30, 2023, 10:09 AM +02:00

--- lecture1/parity.c
+++ lecture1/parity.c
@@ -5,5 +5,15 @@ int main(void)
 {
 
     int n = get_int("n: ");
-    
+    // You can make a pseudocode, retain it or transfer them somewhere
+    // if n is even
+    if (n = even?)
+        {
+            printf("even\n");
+        }
+    // else n is odd, you do not need to put anything on the last conditionals if there are only two options, if 3, last is always empty
+    else
+        {
+            prinf("odd\n");
+        }
 }
