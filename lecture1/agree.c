a1c47ad32cfc191086508ee5e14e296af375295d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 08:51:03 +0000

    Sat, Sep 30, 2023, 10:51 AM +02:00

--- lecture1/agree.c
+++ lecture1/agree.c
@@ -8,7 +8,7 @@ int main(void)
 
     // Check whether user agreed then we make a conditionals if they typer lower or uppercase, || this sign means or, we cannot use & for indicating both as user needs to only input 1 character and not both
     // Notice we use one quote here ' ' which it is for char, if it is string(multiple characters), it should be "", int does not need one
-    // Program won't work with yes or no, but let's try it a bit( we tried doing with get_string but it needs further functions which are available in another library called string.h)
+    // Program won't work with yes or no, but let's try it a bit( we tried doing with get_string but it needs further functions which are available in another library called string.h) There is the strcmp and also strcasecmp which is case sensitive
     if (answer == 'y' || answer == 'Y')
     {
         printf("Agreed.\n");
---
+++ lecture1/meow.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Use simple library as we only need the functions for this one
+
+int main(void)
+{
+    // Now we are tacking LOOPS
+    printf("meow\n")
+}
