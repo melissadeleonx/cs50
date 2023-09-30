1da401d15e7f0f7fcd249c5250740460f5a0a0e8
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 08:33:24 +0000

    Sat, Sep 30, 2023, 10:33 AM +02:00

--- lecture1/agree.c
+++ lecture1/agree.c
@@ -5,7 +5,8 @@ int main(void)
 {
     // Prompt user to agree, if only a single letter, use char or char datatype
     char answer = get_char("Do you agree? ");
-    // Check whether user agreed
+    
+    // Check whether user agreed then we make a conditionals if they typer lower or uppercase, || this sign means or, we cannot use & for indicating both as user needs to only input 1 character and not both
     if (answer == 'y' || answer == 'Y')
     {
         printf("Agreed.\n");
