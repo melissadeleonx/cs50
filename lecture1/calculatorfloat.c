1dbaecf993e26711ac5e4bd73824ee97c20214e6
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 21:13:41 +0000

    Sat, Sep 30, 2023, 11:13 PM +02:00

--- lecture1/calculatorfloat.c
+++ lecture1/calculatorfloat.c
@@ -4,7 +4,7 @@
 // Explain the issue with integer overflow, using a lot of bits but there are not much availability. Computer is only providing the closest approximation.
 // Floating-point imprecision - inability of the computer to represent all possible real numbers - In C, this problem occurs while other scientific languages already found a solution
 // First version of the code, we use float discussing the above topics
-// Second version, we are trying with int data type
+// Second version, we are trying with int data type of x and y with float z. The result of the division is 0.0000 which is called truncation in C(throwing away the decimals)
 int main(void)
 {
 // Prompt user for x
@@ -14,6 +14,6 @@ int main(void)
     int y = get_int("y: ");
 
     // Divide x by y
-    float z = x / y;
-    printf("%.f is the quotient of x by y\n", z);
+    int z = x / y;
+    printf("%i is the quotient of x by y\n", z);
 }
