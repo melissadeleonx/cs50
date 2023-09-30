a024af20cecef08d9e85c5c0e3f6ddc346564978
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 13:19:01 +0000

    Sat, Sep 30, 2023, 3:19 PM +02:00

--- lecture1/mario.c
+++ lecture1/mario.c
@@ -4,5 +4,13 @@
 // Goal to make mario icons, this kind of symbol manipulation is called ASCII art!
 int main(void)
 {
-    printf("????\n");
+    // Use a 'for' loop to make the ? repeat 4 times horizontally - First version of the code
+    // do while LOOP will be discussed now for the second version of the code
+    // do while is similar to the while loop but the condition is at the end, do the right thing but while 
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("?");
+    }
+    printf("\n"); // Put it in the end of the loop so it won't affect the other '?'
 }
