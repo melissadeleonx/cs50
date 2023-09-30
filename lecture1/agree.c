06ad958dd80163500916be0e7ed155718609eb31
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 08:45:13 +0000

    Sat, Sep 30, 2023, 10:45 AM +02:00

--- lecture1/agree.c
+++ lecture1/agree.c
@@ -4,16 +4,16 @@
 int main(void)
 {
     // Prompt user to agree, if only a single letter, use char or char datatype
-   string answer = get_string("Do you agree? ");
+    char answer = get_char("Do you agree? ");
 
     // Check whether user agreed then we make a conditionals if they typer lower or uppercase, || this sign means or, we cannot use & for indicating both as user needs to only input 1 character and not both
     // Notice we use one quote here ' ' which it is for char, if it is string(multiple characters), it should be "", int does not need one
-    // Program won't work with yes or no, but let's try it a bit
-    if (answer == "yes" || answer == "Yes")
+    // Program won't work with yes or no, but let's try it a bit( we tried doing with get_string but it needs further functions which are available in another library called string.h)
+    if (answer == 'y' || answer == 'Y')
     {
         printf("Agreed.\n");
     }
-    else if (answer == "no" || answer == "NO")
+    else if (answer == 'n' || answer == 'N')
     {
         printf("Not agreed.\n");
     }
