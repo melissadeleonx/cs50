2a5dbdfafd6c862cac5adae5fca5575babcdaf33
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 09:08:27 +0000

    Sat, Sep 30, 2023, 11:08 AM +02:00

--- lecture1/meow.c
+++ lecture1/meow.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 // Use simple library as we only need the functions for this one
+// Now we are tacking LOOPS
 
 int main(void)
 {
-    // Now we are tacking LOOPS
     printf("meow\n")
+
+    // while loop also take bool function
 }
