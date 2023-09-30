bd219665e723bb014b0571fc812251e707bc44c0
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 12:09:07 +0000

    Sat, Sep 30, 2023, 2:09 PM +02:00

---
+++ lecture1/function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void meow(void);
+const int n = 3;
+int main(void)
+{
+
+}
+
+void meow(void)
+{
+     for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+        }
+}
