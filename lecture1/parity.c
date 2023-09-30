47d8375683d326198d2f4d230900e1e1103418e5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 08:11:06 +0000

    Sat, Sep 30, 2023, 10:11 AM +02:00

--- lecture1/parity.c
+++ lecture1/parity.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Goal is to identify if number is odd or even, we can use the operator mod % to get the remainder, 0 for even, 1 for odd
 int main(void)
 {
 
