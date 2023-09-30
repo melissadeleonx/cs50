32780f52251537c900939942fee2ef3338f19673
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 21:08:01 +0000

    Sat, Sep 30, 2023, 11:08 PM +02:00

--- lecture1/calculatorfloat.c
+++ lecture1/calculatorfloat.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// Explain the issue with long numbers
+// Explain the issue with integer overflow, using a lot of bits but there are not much availability. Computer is only providing the closest approximation.
+// Floating-point imprecision - inability of the computer to represent all possible real numbers - In C, this problem occurs while other scientific languages already found a solution
+
 int main(void)
 {
 // Prompt user for x
