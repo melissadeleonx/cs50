aff345ca72732599f6e93b889434f527f124d1bc
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 08:15:22 +0000

    Sat, Sep 30, 2023, 10:15 AM +02:00

--- lecture1/parity.c
+++ lecture1/parity.c
@@ -8,13 +8,13 @@ int main(void)
     int n = get_int("n: ");
     // You can make a pseudocode, retain it or transfer them somewhere
     // if n is even
-    if (n = even?)
+    if (n % 2 == 0) // == is the equivalent operator; java even have === :)
         {
             printf("even\n");
         }
     // else n is odd, you do not need to put anything on the last conditionals if there are only two options, if 3, last is always empty
     else
         {
-            prinf("odd\n");
+            printf("odd\n");
         }
 }
