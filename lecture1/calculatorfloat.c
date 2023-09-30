c2fb3fe430245b82cf008b4de2f8c8c7e3bb55ae
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 21:22:35 +0000

    Sat, Sep 30, 2023, 11:22 PM +02:00

--- lecture1/calculatorfloat.c
+++ lecture1/calculatorfloat.c
@@ -5,6 +5,9 @@
 // Floating-point imprecision - inability of the computer to represent all possible real numbers - In C, this problem occurs while other scientific languages already found a solution
 // First version of the code, we use float discussing the above topics
 // Second version, we are trying with int data type of x and y with float z. The result of the division is 0.0000 which is called truncation in C(throwing away the decimals)
+// Third version, this is extra exercise, I want to learn more!  int z = x / y;    int a = x % y;
+// Fourth version, we are disussing a feature called type conversion, we can do it to one of the variables or both to get the floating value of the answer
+
 int main(void)
 {
 // Prompt user for x
@@ -14,7 +17,8 @@ int main(void)
     int y = get_int("y: ");
 
     // Divide x by y
-    int z = x / y;
-    int a = x % y; // This is extra exercise, I want to learn more!
-    printf("%i remainder %i is the answer for x divided by y\n", z, a);
+    float z = x / (float) y;
+
+
+    printf("%.50f\n", z);
 }
