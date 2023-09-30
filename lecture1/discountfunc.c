fdd2da8b4d4d3ff00cdc455155ab9acaad8592cf
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 12:47:57 +0000

    Sat, Sep 30, 2023, 2:47 PM +02:00

--- lecture1/discountfunc.c
+++ lecture1/discountfunc.c
@@ -1,14 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-float discount(int regular);
+float discount(float regular);
 // A modified version of discount.c with our created function called float discount
 
 int main(void)
 {
     float regular = get_float("Regular Price: ");
-    float sale = float discount;
-    printf("The Sale Price is %.2f", discount);
+    float sale = discount;
+    printf("The Sale Price is %.2f", sale);
 
 }
 
@@ -16,5 +16,4 @@ int main(void)
 float discount(int regular)
 {
     float sale = regular * .85;
-    return sale;
 }
