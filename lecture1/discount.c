8be5f0fa42e8f3eb6301c59d6e3d4311cff3d7b5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 12:32:11 +0000

    Sat, Sep 30, 2023, 2:32 PM +02:00

--- lecture1/discount.c
+++ lecture1/discount.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Use clear to clear terminal
+// Now, let's try to make other functions with data types
+
 int main(void)
 {
-    float regular = get_float("")
+    // Prompt user to input regular price using the data type float for numbers with decimals
+    float regular = get_float("Regular Price: ");
+
+    float sale = regular * .85;
+    printf("The discount price is %.1f\n", sale);
 }
