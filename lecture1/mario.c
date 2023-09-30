0ef11dd81be48b8e522d2c0a0a4d48965b4f07d8
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 14:37:58 +0000

    Sat, Sep 30, 2023, 4:37 PM +02:00

--- lecture1/mario.c
+++ lecture1/mario.c
@@ -7,8 +7,8 @@ int main(void)
     int n;
     // Use a 'for' loop to make the ? repeat 4 times horizontally - First version of the code
     // do while LOOP will be discussed now for the second version of the code
-    // do while is similar to the while loop but the condition is at the end, do the right thing but while indicates a new condition and what should be done for this new condition
-    // For the second version of the code, we need to make a brick
+    // do while is similar to the while loop but the condition is at the end, do the right thing and while will prompt user again and again until do is met
+    // For the second version of the code, we need to make a brick of #
     do
     {
         n = get_int("Size: "); // The size in this context is the
@@ -16,8 +16,12 @@ int main(void)
 
    for (int i = 0; i < n; i++)
    {
+    for (int j = 0; j < n; j++)
+       {
     printf("#");
    }
+   }
+
    printf("\n");
 
     // printf("\n") Put it in the end of the loop so it won't affect the other ASCII art
