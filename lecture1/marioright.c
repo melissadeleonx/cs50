bf4a0afa3375d38449b5d8cd62a94534b9353011
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 15:21:48 +0000

    Sat, Sep 30, 2023, 5:21 PM +02:00

--- lecture1/marioright.c
+++ lecture1/marioright.c
@@ -4,17 +4,19 @@
 // Goal is to make a right pyramid #
 int main(void)
 {
+    // First declare n as the size input, meaning the largest height and width of the pyramid
     int n;
     do
     {
-        n = get_int("Size: "); 
+        n = get_int("Size: ");
     } while (n < 1);
 
+    //
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i + 1; j++) // (--) always lead me to error, lol!
+        for (int j = 0; j < i + 1; j++)
         {
-            printf("#");
+            printf(" ");
         }
         printf("\n");
     }
