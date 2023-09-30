c034cf881ba34ddfa1634904bb004c68458fd771
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 11:56:00 +0000

    Sat, Sep 30, 2023, 1:56 PM +02:00

--- lecture1/meowfor.c
+++ lecture1/meowfor.c
@@ -6,10 +6,10 @@ int main(void)
 {
     // Initialize the value of i and its datatype!!(which always start depending on the programmer with 0) and indicate its paramenter or limitation like, should i be less than or greater than a certain number
     // Try on your own
-    for (int i = 0; i < 10; i--)
+    for (int i = 0; i <= 10; i++)
     {
         printf("meow\n");
      // no need for now to put the increments here
-     // try to do -- :
+     // try to do -- : the variable i(count) will loop infinitely as long as it does not reach 10 making it smaller but goes on and on and on
     }
 }
