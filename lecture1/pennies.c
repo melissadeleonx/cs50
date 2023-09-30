3dfdb4571a8e7b192ad9a7ba07a188e0bdb04de3
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-09-30 21:41:48 +0000

    Sat, Sep 30, 2023, 11:41 PM +02:00

--- lecture1/pennies.c
+++ lecture1/pennies.c
@@ -4,6 +4,7 @@
 
 // First version has 4.20 dollar amount to pennies but only resulted to 419 pennines, programmers need to be attended to this kind of problem
 // We added the math.h library for a function called round and it works correctly
+// These undefined behavior can crash an airplance so be careful
 int main(void)
 {
     float amount =  get_float("Dollar amount: ");
