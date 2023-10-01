c089969202b27355cc90a7937371eb0f47392297
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 16:17:05 +0000

    Sun, Oct 1, 2023, 6:17 PM +02:00

--- CS50/lecture2/scores.c
+++ CS50/lecture2/scores.c
@@ -6,5 +6,5 @@ int main (void)
     int score2 = 73;
     int score3 = 33;
 
-    printf("Average: %f\n", ((float)score1 + score2 + score3) / 3); // typecasting is the solution to get a float result of integers or we can also put a decimal .0 on the number 3. As long as one of the values is float, all will be treated as float
+    printf("Average: %f\n", ((float)score1 + score2 + score3) / 3); // typecasting is the solution to get a float result of integers or we can also put a decimal .0 on the number 3(but not ideal). As long as one of the values is float, all will be treated as float
 }
