2cb32cfa3e26b7e20076155f2bae3efea2abc492
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 06:21:54 +0000

    Sun, Oct 1, 2023, 8:21 AM +02:00

--- CS50/lecture2/helloclang.c
+++ CS50/lecture2/helloclang.c
@@ -8,7 +8,9 @@ int main(void)
     // if you use clang, the terminal will have errors with get_string as undefined, why?
     // because there is another way for the computer to find already compiled 0 and 1 by the authors of a specific library like CS50
     // so we need to manually tell clang not only we want to o(output) a file and compile(.c), we also want to link in library with bunch of 1 and 0
-    // clang -o helloclang helloclang.c lcs50
+    // clang -o helloclang helloclang.c -lcs50
+    // Basically, we need to indicate all libraries that we want to add to compile the files on the termina
+    // I also 
 
     printf("Hello %s!\n", name);
 }
