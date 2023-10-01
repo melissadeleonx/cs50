a0daf1628a103b54d06e0c93c312d3886fce7acf
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 12:44:20 +0000

    Sun, Oct 1, 2023, 2:44 PM +02:00

--- CS50/lecture2/buggy.c
+++ CS50/lecture2/buggy.c
@@ -5,6 +5,8 @@
 // How debugger works, you can use for now debug50 - compile first before debugging and make sure to put a breakpoint on the line you want to debug. There are other parts of the debugger like Step In Step Out etc.
 // What is the alternative for debug50??
 // Best debugger method is talking to someone or your imaginary companion. Works magic! I did it for the brick problems!
+
+
 int main(void)
 {
     for (int i = 0; i < 3; i++)
