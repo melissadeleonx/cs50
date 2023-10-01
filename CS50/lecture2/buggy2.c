963c9f0c9b0acfff62cf1e84356b8db8f33cd1c5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 12:46:58 +0000

    Sun, Oct 1, 2023, 2:46 PM +02:00

---
+++ CS50/lecture2/buggy2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+get_negative_int();
+
+int main(void)
+{
+    int i = get_negative_int();
+    printf("%i\n", i);
+
+}
+
+
+
+get_negative_int(integer )
+{
+
+}
