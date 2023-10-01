fde094984946f4760b0b0fc6916ec0e4a3e0c499
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 08:31:52 +0000

    Sun, Oct 1, 2023, 10:31 AM +02:00

--- CS50/lecture2/helloclang.c
+++ CS50/lecture2/helloclang.c
@@ -1,12 +1,10 @@
-#include <cs50.h>
 #include <stdio.h>
 
 // Compile with Clang instead of the automated make command
 // First version of the code, we include cs50.h and learn about clang -o helloclang helloclang.c -lcs50
-// Second version
+// Second version, let us only use the stdio.h library which is a very very standard library with lots of prototype functions like printf
 int main(void)
 {
-    string name = get_string("What's your name?  ");
     // if you use clang, the terminal will have errors with get_string as undefined, why?
     // because there is another way for the computer to find already compiled 0 and 1 by the authors of a specific library like CS50
     // so we need to manually tell clang not only we want to o(output) a file and compile(.c), we also want to link in library with bunch of 1 and 0
