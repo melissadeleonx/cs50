0964442f54b862a3acc2d13196ed6e00ca5821c4
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 16:55:50 +0000

    Sun, Oct 1, 2023, 6:55 PM +02:00

--- CS50/lecture2/scores.c
+++ CS50/lecture2/scores.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 // Get the average of the scores - the numbers represent hi! in ASCII
+// First version, we use 3 different variables with almost the same name
+// Second version, we will use the type of data called ARRAY - 
 int main (void)
 {
     int score1 = 72;
