c124d2b7945cd5d156becb90454ba7309a0dcee8
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 12:43:33 +0000

    Sun, Oct 1, 2023, 2:43 PM +02:00

--- CS50/lecture2/buggy.c
+++ CS50/lecture2/buggy.c
@@ -2,9 +2,9 @@
 
 // Debugging Exercises - I corrected the bug manually but let's see what other procedure can we follow by putting < 3 and separating # from \n
 // Use printf to detect the bug, it is pretty cool to see the i(counter times)
-// How debugger works, you can use for now debug50 - compile first before debugging and make sure to put a breakpoint on the line you want to debug
+// How debugger works, you can use for now debug50 - compile first before debugging and make sure to put a breakpoint on the line you want to debug. There are other parts of the debugger like Step In Step Out etc.
 // What is the alternative for debug50??
-
+// Best debugger method is talking to someone or your imaginary companion. Works magic! I did it for the brick problems!
 int main(void)
 {
     for (int i = 0; i < 3; i++)
