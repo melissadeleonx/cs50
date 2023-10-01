e18e6cca0f5e6c6554b09f15990c572570df537e
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 20:56:56 +0000

    Sun, Oct 1, 2023, 10:56 PM +02:00

--- CS50/lecture2/scores.c
+++ CS50/lecture2/scores.c
@@ -2,12 +2,12 @@
 
 // Get the average of the scores - the numbers represent hi! in ASCII
 // First version, we use 3 different variables with almost the same name
-// Second version, we will use the type of data called ARRAY - 
+// Second version, we will use the type of data called ARRAY - allows us to store multiple values in the same variable
 int main (void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    // Use [] sign to declare how much 
+    int scores[3];
+    scores[0]
 
     printf("Average: %f\n", ((float)score1 + score2 + score3) / 3); // typecasting is the solution to get a float result of integers or we can also put a decimal .0 on the number 3(but not ideal). As long as one of the values is float, all will be treated as float
 }
