4b64a7ab913bf481c265ac5d9def0494ffeec955
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 06:14:35 +0000

    Sun, Oct 1, 2023, 8:14 AM +02:00

--- CS50/lecture2/helloclang.c
+++ CS50/lecture2/helloclang.c
@@ -5,5 +5,7 @@
 int main(void)
 {
     string name = get_string("What's your name?  ");
+    // if you use clang, the terminal will have errors with get_string as undefined, why?
+
     printf("Hello %s!\n", name);
 }
