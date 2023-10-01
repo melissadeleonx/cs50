896a8e30c6b430f63c89c9c1a4ddf0248daf5e8a
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 06:39:41 +0000

    Sun, Oct 1, 2023, 8:39 AM +02:00

--- CS50/lecture2/helloclang.c
+++ CS50/lecture2/helloclang.c
@@ -9,8 +9,8 @@ int main(void)
     // because there is another way for the computer to find already compiled 0 and 1 by the authors of a specific library like CS50
     // so we need to manually tell clang not only we want to o(output) a file and compile(.c), we also want to link in library with bunch of 1 and 0
     // clang -o helloclang helloclang.c -lcs50
-    // Basically, we need to indicate all libraries that we want to add to compile the files on the termina
-    // I also 
+    // Basically, we need to indicate all libraries(math, string, etc.) that we want to add to compile the files on the termina
+    // I also tried to so the make command on VSCode Desktop and it is quite a long process to make 'make' command word
 
     printf("Hello %s!\n", name);
 }
