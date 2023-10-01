e8cffa589ab69c61d26bf66dc7cb65f23abdf4d5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 21:34:57 +0000

    Sun, Oct 1, 2023, 11:34 PM +02:00

---
+++ CS50/lecture2/char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Goal is to learn more about the data type char = ' '; Each letters, numbers and symbols are characters
+// With char, letters can be converted to numbers and from decimal numbers to binary numbers, the machine code of the computer
+int main(void)
+{
+    char C1 = 'H';
+    char C2 = 'I';
+    char C3 = '!';
+
+    printf("%c%c%c\n", C1, C2, C3);
+}
+
