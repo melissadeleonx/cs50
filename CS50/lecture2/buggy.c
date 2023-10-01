3d872d04a85bf37ecd8e1ccbdf02831ac87cd4b9
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 12:41:08 +0000

    Sun, Oct 1, 2023, 2:41 PM +02:00

--- CS50/lecture2/buggy.c
+++ CS50/lecture2/buggy.c
@@ -2,11 +2,12 @@
 
 // Debugging Exercises - I corrected the bug manually but let's see what other procedure can we follow by putting < 3 and separating # from \n
 // Use printf to detect the bug, it is pretty cool to see the i(counter times)
-// How debugger works, you can us for now debug50
+// How debugger works, you can use for now debug50 - compile first before debugging and make sure to put a breakpoint on the line you want to debug
+// What is the alternative for debug50??
 
 int main(void)
 {
-    for (int i = 0; i <= 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         printf("#\n");
     }
