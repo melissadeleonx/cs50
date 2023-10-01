32a9266a6490d09b9ecb201aaabb4c4e3bff62af
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-01 09:53:19 +0000

    Sun, Oct 1, 2023, 11:53 AM +02:00

--- CS50/lecture2/buggy.c
+++ CS50/lecture2/buggy.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+// Debugging Exercises - I corrected the bug manually but let's see what other procedure can we follow by putting < 3 and separating # from \n
+// Use printf to detect the bug, it is pretty cool to see the i(counter times)
+
 int main(void)
 {
     for (int i = 0; i <= 3; i++)
     {
+        printf("i is %i\n", i);
         printf("#\n");
     }
 }
