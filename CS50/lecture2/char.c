b116062251076e730ffffb80a06d3000a7b21ee8
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 08:48:33 +0000

    Mon, Oct 2, 2023, 10:48 AM +02:00

--- CS50/lecture2/char.c
+++ CS50/lecture2/char.c
@@ -10,7 +10,7 @@
 // Third version is now using string HI! Do not forget the library that support string which is CS50.h
 // String is an array of characters as proven by the first and second versions
 // Fourth version is proving the definition of string with arrays string[0][1][2]. . .
-// Now we are too technical, lol, \0 or NUL(ASCII) is being introduced, it determine when a string is to end or start. 8 0 bits means nul(\0)
+// Now we are too technical, lol, \0 or NUL(ASCII) is being introduced, it determine when a string is to end or start. 8 0 bits means nul(\0). It is an invisible placeholder.
 int main(void)
 {
     string s = "HI!";
