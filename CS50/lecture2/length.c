1c344cb59af74664d304c81665bc48c1815db4c8
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 09:20:34 +0000

    Mon, Oct 2, 2023, 11:20 AM +02:00

--- CS50/lecture2/length.c
+++ CS50/lecture2/length.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
 
-// Write a pseudocode(i in this context is the length of the string)
+// Write a pseudocode(int i in this context is the length of the string)
 int main(void)
 {
     string name = get_int("Name: ");
 
     int i = 0;
+    while(name[i] != '\0') // We are basically excluding the last string array which is nul to get the lenght of the string, there is also another datatype called strlen to make it easier for us
+    {
+        
+    }
 }
