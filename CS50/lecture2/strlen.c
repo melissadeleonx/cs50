fe97eadb789e06bd7ebc89dea39f58bca7ca1516
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 14:35:03 +0000

    Mon, Oct 2, 2023, 4:35 PM +02:00

--- CS50/lecture2/strlen.c
+++ CS50/lecture2/strlen.c
@@ -2,11 +2,14 @@
 #include <cs50.h>
 #include <string.h> // string.h library has the strlen function that counts the number of char in string, automatically excluding '\0' nul
 
+// Learn about strlen and try to manipulate more examples
 int main(void)
 {
-    string name = get_string("What is your name? ");
+    string name1 = get_string("What is your name? ");
+    string name2 = get_string("Name: ");
 
-    int n = strlen(name);
-    printf("%i\n", n);
+    int n = strlen(name1);
+    int a = strlen(name2);
+    printf("%i is for name1 while %i is for name2\n", n, a);
 
 }
