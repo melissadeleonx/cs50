a9f3f23bbe94574ac82bb3ac58b2e8e7ef786723
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 14:47:46 +0000

    Mon, Oct 2, 2023, 4:47 PM +02:00

--- CS50/lecture2/strlenarray.c
+++ CS50/lecture2/strlenarray.c
@@ -4,5 +4,18 @@
 
 int main(void)
 {
-    
+    int i = 0;
+    string name[3];
+    while(i < 3)
+    {
+        name[i] = get_string("Name: ");
+        i++;
+    }
+
+
+    int n = strlen(name[i]);
+    for (i = 0; i < 3; i++)
+    {
+       printf("The length of %s is %i while the length of %s is %i", name, n );
+    }
 }
