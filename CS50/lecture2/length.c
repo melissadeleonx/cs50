72f79fb20f89fb13c1e40ef7c0281585d471bc89
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 09:36:26 +0000

    Mon, Oct 2, 2023, 11:36 AM +02:00

--- CS50/lecture2/length.c
+++ CS50/lecture2/length.c
@@ -3,17 +3,18 @@
 
 // Write a pseudocode(int i in this context is the length of the string)
 // First version, we use the while loop to get the lenght of the string. I want to try the other types of loops; for loop will just increment it as 0 1 2 3 4 5 6 and not really indicate the total
-//
+// Do while loop also works the same as while, make sure to put ' ' on the \O nul character
+// If you want to get the total of i, put the printf outside the loop
+// Let's put it back to while again.
 int main(void)
 {
     string name = get_string("Name: ");
     int i = 0;
-    do
+
+    while (name[i] != '\0')
     {
         i++;
     }
-    while (name[i] != \0);
-
     printf("%i\n", i);
 
 }
