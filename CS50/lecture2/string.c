a72a192799fa41fb7777ea53a5cd84bec092f761
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 17:07:30 +0000

    Mon, Oct 2, 2023, 7:07 PM +02:00

--- CS50/lecture2/string.c
+++ CS50/lecture2/string.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 #include <string.h>
 
-// Simplify the code with strlen(do not forget the string.h library)
+// Simplify the code with strleclearn(do not forget the string.h library)
 // One can omit int length and use %lu(undefined long) instead of %i
 // Explore the https://manual.cs50.io/ STUDY!!!
 
