3736cd1c862ec3e646f74b653c3c4ab4fd7e4421
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 09:17:27 +0000

    Mon, Oct 2, 2023, 11:17 AM +02:00

---
+++ CS50/lecture2/length.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+// Write a pseudocode(i in this context is the length of the string)
+int main(void)
+{
+    string name = get_int("Name: ");
+
+    int i = 0;
+}
