18cb235dc0f120146c9733eca8317a6fbdd88049
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 12:11:36 +0000

    Mon, Oct 2, 2023, 2:11 PM +02:00

--- CS50/lecture2/stringarray.c
+++ CS50/lecture2/stringarray.c
@@ -4,13 +4,13 @@
 int main(void)
 {
     int i = 0;
-    string name[3];
+    string name[i];
     do
     {
         name[i] = get_string("Name: ");
         i++;
-    } while ( name[i] < 3);
+    } while ( i < 3);
 
-    printf("The name of my friends are %s, %s, %s", name[0], name[1], name[2]);
+     printf("The name of my friends are %s", name[3]);
 
 }
