74ac6c48ad500c48ad01669872f50248f5d73947
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 16:38:01 +0000

    Mon, Oct 2, 2023, 6:38 PM +02:00

--- CS50/lecture2/strlenarray.c
+++ CS50/lecture2/strlenarray.c
@@ -6,11 +6,22 @@
 int main(void)
 {
     // We need 3 names in th same variable. Printf to check!
+int main(void)
+{
     string name[3];
     for (int i = 0; i < 3; i++)
     {
         name[i] = get_string("Name: ");
     }
+        printf("You've entered: ");
+        int i = 0;
+        while (i < 3)
+        {
+            printf(" %s", strlen(name[i]));
+            i++;
+        }
+        printf("\n"); // Put the next line outside the loop so it won't repeat itself
+}
 
 
 }
