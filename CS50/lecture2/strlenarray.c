11b8e97b61a712b1790d349fcbe507977b58a10f
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 16:40:22 +0000

    Mon, Oct 2, 2023, 6:40 PM +02:00

--- CS50/lecture2/strlenarray.c
+++ CS50/lecture2/strlenarray.c
@@ -3,9 +3,7 @@
 #include <string.h>
 
 // Always start with pseudocode so you won't make a mistake, solve one at a time
-int main(void)
-{
-    // We need 3 names in th same variable. Printf to check!
+// We need 3 names in th same variable. Printf to check!
 int main(void)
 {
     string name[3];
@@ -14,10 +12,11 @@ int main(void)
         name[i] = get_string("Name: ");
     }
         printf("You've entered: ");
+        int sl = strlen name[i];
         int i = 0;
         while (i < 3)
         {
-            printf(" %s", strlen(name[i]));
+            printf(" %s%i", name[0], sl(name[0]));
             i++;
         }
         printf("\n"); // Put the next line outside the loop so it won't repeat itself
