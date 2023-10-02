47c24f529f6e27c577c32b50be9a9b98311a96c6
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 20:05:47 +0000

    Mon, Oct 2, 2023, 10:05 PM +02:00

--- CS50/lecture2/uppercase.c
+++ CS50/lecture2/uppercase.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
+
 // Goal to turn before lowercase to uppercase letters(char)
+// I used the function tolower from ctype.h library. Let's hear what Dr. Malan propose?
+
 
 int main(void)
 {
