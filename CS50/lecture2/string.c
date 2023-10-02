3443034e15dc12841e95e3bfdbffcd7ac9376585
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 17:01:33 +0000

    Mon, Oct 2, 2023, 7:01 PM +02:00

--- CS50/lecture2/string.c
+++ CS50/lecture2/string.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
-
+// Simplify the code with strlen(do not forget the string.h library)
 int main(void)
 {
     string name = get_string("Name: ");
     int length = strlen(name);
-    printf("Length is %i", length);
+    printf("Length is %i\n", length);
 }
