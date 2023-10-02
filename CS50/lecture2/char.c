d1e7f0b11ec2269087ba7818595a9f8b669f942d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 08:40:06 +0000

    Mon, Oct 2, 2023, 10:40 AM +02:00

--- CS50/lecture2/char.c
+++ CS50/lecture2/char.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <cs50.h>
 
 // Goal is to learn more about the data type char = ' '; Each letters, numbers and symbols are characters
 // With char, letters can be converted to numbers and from decimal numbers to binary numbers, the machine code of the computer
@@ -6,10 +7,11 @@
 // Second version, using ASCII, we will convert letters to numbers but still printing the char[count of string]
 // Good to note that for char letters, we put ' ' and with numbers there is no need. For char numbers, the representation is %i too.
 // C knows ASCII and Unicode so it recognizes the conversion
-// Third version is now using string HI!
+// Third version is now using string HI! Do not forget the library that support string which is CS50.h
+// String is an array of characters as proven by the first and second versions
 int main(void)
 {
-    string hi = "HI!";
-    printf("%s", hi);
+    string s = "HI!";
+    printf("%s\n", s);
 }
 
