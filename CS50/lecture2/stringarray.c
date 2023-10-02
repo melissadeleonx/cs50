b5d2f78a9f0177a16b125e65d259e069a5b57e1d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 12:14:33 +0000

    Mon, Oct 2, 2023, 2:14 PM +02:00

--- CS50/lecture2/stringarray.c
+++ CS50/lecture2/stringarray.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <cs50.h>
 
+// Goal is to make an array of string, similar to the array int on score.c
+// First version is a copied code online for study reference, check on the terminal and try to remember the rules
+
 int main(void)
 {
+    // Define an array of strings
+    string myStrings[3];
+
+    // Get strings from the user using a while loop
     int i = 0;
-    string name[i];
-    do
+    while (i < 3)
     {
-        name[i] = get_string("Name: ");
+        myStrings[i] = get_string("Enter a string: ");
         i++;
-    } while ( i < 3);
+    }
 
-     printf("The name of my friends are %s", name[3]);
+    // Print the entered strings
+    printf("You entered:\n");
+    i = 0;
+    while (i < 3)
+    {
+        printf("%s\n", myStrings[i]);
+        i++;
+    }
 
+    return 0;
 }
