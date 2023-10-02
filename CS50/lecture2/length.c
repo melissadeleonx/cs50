81a3c85c997491407d37d308e32a5016d0cf2245
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 09:32:41 +0000

    Mon, Oct 2, 2023, 11:32 AM +02:00

--- CS50/lecture2/length.c
+++ CS50/lecture2/length.c
@@ -2,13 +2,18 @@
 #include <cs50.h>
 
 // Write a pseudocode(int i in this context is the length of the string)
-// First version, we use the while loop to get the lenght of the string. I want to try the other types of loops
+// First version, we use the while loop to get the lenght of the string. I want to try the other types of loops; for loop will just increment it as 0 1 2 3 4 5 6 and not really indicate the total
+//
 int main(void)
 {
     string name = get_string("Name: ");
-    for (int i = 0; name[i] != '\0'; i++)
+    int i = 0;
+    do
     {
-
-        printf("%i\n", i);
+        i++;
     }
+    while (name[i] != \0);
+
+    printf("%i\n", i);
+
 }
