09d4948e043d33d8b22339ea73c372097c0da26c
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 11:35:55 +0000

    Mon, Oct 2, 2023, 1:35 PM +02:00

--- CS50/lecture2/length.c
+++ CS50/lecture2/length.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <cs50.h>
+#include <cs    50.h>
 
 // Write a pseudocode(int i in this context is the length of the string)
 // First version, we use the while loop to get the lenght of the string. I want to try the other types of loops; for loop will just increment it as 0 1 2 3 4 5 6 and not really indicate the total
@@ -29,5 +29,5 @@ int string_length(string name)
     {
         i++;
     }
-    return i;
+    return i; // not print it because the return value will be used on the main code
 }
