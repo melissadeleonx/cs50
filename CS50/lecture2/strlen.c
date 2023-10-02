8304ffb467ccda8cf179329db9a53a6c943d641c
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-02 10:26:56 +0000

    Mon, Oct 2, 2023, 12:26 PM +02:00

--- CS50/lecture2/strlen.c
+++ CS50/lecture2/strlen.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <string.h>
+#include <string.h> // string.h library has the strlen function that counts the number of char in string, automatically excluding '\0' nul
 
 int main(void)
 {
