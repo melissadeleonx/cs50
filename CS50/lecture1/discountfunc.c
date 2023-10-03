fd3cdaa6b880bd3c555579c1bcc96a54c7b651b7
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 23:04:53 +0000

    Wed, Oct 4, 2023, 1:04 AM +02:00

--- CS50/lecture1/discountfunc.c
+++ CS50/lecture1/discountfunc.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-float discount(float regular, int percentage);
+float discount(float regular, int percent_off);
 // A modified version of discount.c with our created function called float discount
 // Sometimes, the SIMPLER CODES are the BETTER CODES
 // Watch out for the data types inside the () of the new functions, the return of the input, for this case, the prompted regular from the user
