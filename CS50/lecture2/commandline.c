7541057d16d0f5ba8a1c04024df830db34bfc654
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 09:32:01 +0000

    Tue, Oct 3, 2023, 11:32 AM +02:00

--- CS50/lecture2/commandline.c
+++ CS50/lecture2/commandline.c
@@ -3,7 +3,16 @@
 #include <string.h>
 
 // Goal is to set simple programs with commandline arguments
+// Commandline arguments are the basics of programming and getting input from users from the main code
+// Let's play around one more
 int main(int argc, string argv[])
 {
-    printf("Hello %s\n", argv[200000]);
+    if (argc == 3)
+    {
+    printf("Hello %s %s\n", argv[1], argv[2]);
+    }
+    else
+    {
+        printf("Hello World!\n");
+    }
 }
