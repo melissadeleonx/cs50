cd73441b40d4e568f77e0d5b340ad354e03484e9
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 21:58:35 +0000

    Tue, Oct 3, 2023, 11:58 PM +02:00

--- CS50/psets1/half.c
+++ CS50/psets1/half.c
@@ -2,13 +2,24 @@
 #include <math.h>
 #include <stdio.h>
 
-// Goal: Calculates exactly the share of of the bill after tax and tip are added, and returns that value.
+// Calculate your half of a restaurant bill
+
+float half(float bill, float tax, int tip);
+
+// Data types, operations, type casting, return value
 // Step 1: Make a pseudocode.
 int main(void)
 {
     // Prompt user to enter the bill amount before tax and tip
-    float Bill_before_tax_and_tip = get_float("Bill before tax and tip: ");
-    float
+{
+    float bill_amount = get_float("Bill before tax and tip: ");
+    float tax_percent = get_float("Sale Tax Percent: ");
+    int tip_percent = get_int("Tip percent: ");
 
+    float bill_tax = bill + (tax/bill);
+    float half = (bill_tax + (tip/bill_tax)) / 2;
 
+    printf("You will owe %.2f each!\n", half);
+}
+    return 0;
 }
