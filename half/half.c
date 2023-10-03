e6d5effebdc099236f982e766257c0abe42e92cc
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 23:56:48 +0000

    Wed, Oct 4, 2023, 1:56 AM +02:00

--- half/half.c
+++ half/half.c
@@ -1,9 +1,8 @@
-// Calculate your half of a restaurant bill
-// Data types, operations, type casting, return value
-
 #include <cs50.h>
 #include <stdio.h>
 
+// Calculate your half of a restaurant bill
+// Data types, operations, type casting, return value
 float half(float bill, float tax, int tip);
 
 int main(void)
