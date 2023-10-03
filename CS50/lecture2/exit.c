b4a6788680a0004501b3624e5c8ce8c1323fd725
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 10:35:52 +0000

    Tue, Oct 3, 2023, 12:35 PM +02:00

--- CS50/lecture2/exit.c
+++ CS50/lecture2/exit.c
@@ -4,6 +4,8 @@
 
 // Learn about exit status, a feature in main. It is an ability to signal user whether something was successful or not
 // Let's make an example again with command-lines int argc and string argv
+// When doing conditionals, sometimes we can skip else and just do the regular code after if
+// Main is now returning something for us, now codes are getting more sophisticated, what to do next?
 int main(int argc, string argv[]) // Do not forget [] on argv
 {
     // Implement the conditionals, do not confuse the number of counts to number of argv which in coding always start with 0, it means here, 2 = argv[0] and argv[1]
@@ -11,9 +13,7 @@ int main(int argc, string argv[]) // Do not forget [] on argv
     {
         printf("Missing command-line argument\n");
         return 1; // This is the exit status. Anytime something is wrong and the conditionals are not implemented correctly, return a non zero value to main
-    } else
-    {
-        printf("Hello %s\n", argv[1]);
-        return 0; // Signal to 
     }
+        printf("Hello %s\n", argv[1]);
+        return 0; // Signal the computer that everything is okay
 }
