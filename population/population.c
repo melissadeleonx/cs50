0b034810bee6d76fa88dd839792b9a591610473c
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 13:37:51 +0000

    Tue, Oct 3, 2023, 3:37 PM +02:00

--- population/population.c
+++ population/population.c
@@ -1,30 +1,49 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
+#include <math.h>
 
+// Step 1: Make a pseudocode
+// Goal: Determine how long it takes for a population to reach a particular size. We need to print the year!
 int main(void)
 {
-    int n;
+    // Prompt the user for a starting population size
+    int start;
+    // Step 2: Make a condition with this rule: If the user enters a number less than 9 (the minimum allowed population size), the user should be re-prompted to enter a starting population size until they enter a number that is greater than or equal to 9. (If we start with fewer than 9 llamas, the population of llamas will quickly become stagnant!)
+    do
+    {
+        start = get_int("Start Size: ");
+        // Put another conditionals inside the loop so there is a prompt message everytime the user puts less than 9
+        if (start < 9 )
+        {
+        printf("Please enter a number equals or less than 9\n");
+        }
+    } while (start < 9);
 
+    // Prompt the user for an ending population size
+    int end;
     do {
-        n = get_int("Enter a starting population size: ");
+        end = get_int("End Size: ");
 
-        if (n <= 9) {
-            printf("Please enter a starting population size greater than 9.\n");
+        // Step 3: Make a condition with this rule: If the user enters a number less than the starting population size, the user should be re-prompted to enter an ending population size until they enter a number that is greater than or equal to the starting population size. (After all, we want the population of llamas to grow!)
+        if (end < start)
+        {
+        printf("Please enter a number greater than the starting population size\n");
         }
-    } while (n <= 9);
+    } while (end < start);
 
-    int x = get_int("Number of newborn llamas: ");
-    int y;
-     do {
-        y = get_int("Number of deceased llamas: ");
+    // Calculate the user input: Your program should then calculate the (integer) number of years required for the population to reach at least the size of the end value.
+    // Remember the formula provided and think of another to satisfy the result. First year = start + 1/3 new llamas - 1/4 deceased llamas
+    int first_year = start + (start/3) - (start/4);
+    int year = ;
 
-        if (y >= n) {
-            printf("Please enter a number less than the population size.\n");
-        }
-    } while (y >= n);
+    printf("Year: %i\n", year);
 
-    int end = n + x - y;
-    printf("The number of llamas at the end of the year is %i\n", end);
-
-    return 0;
 }
+
+
+
+
+
+
+
+    // Print the number of years required for the llama population to reach that end size, as by printing to the terminal Years: n, where n is the number of years.
