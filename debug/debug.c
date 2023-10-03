8b5ce457c541b685c5426b5d2dbfdd1912d1f735
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 17:14:01 +0000

    Tue, Oct 3, 2023, 7:14 PM +02:00

--- debug/debug.c
+++ debug/debug.c
@@ -8,9 +8,10 @@ int main(void)
 {
     // Ask for your name and where live
     string name = get_string("What is your name? ");
-    string location = get_string("Where do you live? ");
+    string address = get_string("Where do you live? ");
 
     // Say hello
-    printf("Hello, %s, from %s!\n", name, location);
+    printf("Hello %s from %s!\n", name, address);
+
     return 0;
     }
