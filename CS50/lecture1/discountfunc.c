a3ac8cf07e3b5cf6021994442f32c96f6e0e21c8
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 23:14:27 +0000

    Wed, Oct 4, 2023, 1:14 AM +02:00

--- CS50/lecture1/discountfunc.c
+++ CS50/lecture1/discountfunc.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-float discount(float regular, int percentage);
+float discount(float regular, int percent_off);
 // A modified version of discount.c with our created function called float discount
 // Sometimes, the SIMPLER CODES are the BETTER CODES
 // Watch out for the data types inside the () of the new functions, the return of the input, for this case, the prompted regular from the user
@@ -11,7 +11,7 @@ int main(void)
 {
     float regular = get_float("Regular Price: ");
     int percentage = get_int("Percent Off: ");
-    float sale = discount(regular, percentage);
+    float sale = discount(float regular, int percent_off);
     printf("The Sale Price is $%.2f\n", sale);
 
 }
@@ -24,9 +24,10 @@ int main(void)
 
 
 // The new functions is not printing anything but handling a value to a variable to make the code cleaner
-float discount(float regular, int percentage)
+float discount(float regular, int percent_off)
 {
-    return regular - (percentage / 100);
+
+    regular - (percent_off / 100);
 
    // I am not sure why Prof. David did not see the bug or why the system made it work but he declared 2 different int, one int percentage and one is int percent_off: I have no idea!
    // Regarding my question, turned out there is a thing called parameters within the functions, which can be changed on the main
