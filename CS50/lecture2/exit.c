419aaa301cd9f89d7814bc040caaabf6aacb6e41
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 09:56:13 +0000

    Tue, Oct 3, 2023, 11:56 AM +02:00

---
+++ CS50/lecture2/exit.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Learn about exit status, a feature in main. It is an ability to signal user whether something was successful or not
+
+int main(void)
+{
+
+}
