190d7f4e2ec70127d84d8ac2046d6542870857e5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 11:07:55 +0000

    Tue, Oct 3, 2023, 1:07 PM +02:00

--- CS50/psets1/population.c
+++ CS50/psets1/population.c
@@ -7,6 +7,11 @@ int main(void)
 {
     // Prompt the user for a starting population size
 
-    // 
+    // Step 2: Make a condition with this rule: If the user enters a number less than 9 (the minimum allowed population size), the user should be re-prompted to enter a starting population size until they enter a number that is greater than or equal to 9. (If we start with fewer than 9 llamas, the population of llamas will quickly become stagnant!)
 
+    // Prompt the user for an ending population size
+
+    // Step 3: Make a condition with thsi rule: If the user enters a number less than the starting population size, the user should be re-prompted to enter an ending population size until they enter a number that is greater than or equal to the starting population size. (After all, we want the population of llamas to grow!)
+
+    
 }
