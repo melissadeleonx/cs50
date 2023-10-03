7d2d328ef47dce8312be624551b99c542f1e752a
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 14:20:42 +0000

    Tue, Oct 3, 2023, 4:20 PM +02:00

--- CS50/psets1/population.c
+++ CS50/psets1/population.c
@@ -33,10 +33,13 @@ int main(void)
 
     // Calculate the user input: Your program should then calculate the (integer) number of years required for the population to reach at least the size of the end value.
     // Remember the formula provided and think of another to satisfy the result. First year = start + 1/3 new llamas - 1/4 deceased llamas
+
+
     int first_year = start + (start/3) - (start/4);
+    int gains = start - first_year;
     int year = (end - start) / (first_year - start);
 
-    printf("Year: %i\n", year);
+    printf("Year: %i\n", gains);
 
 }
 
