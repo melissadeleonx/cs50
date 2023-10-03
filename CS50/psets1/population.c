4bc79a10db5978c488c66af38b6376070aa982b7
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 11:11:35 +0000

    Tue, Oct 3, 2023, 1:11 PM +02:00

--- CS50/psets1/population.c
+++ CS50/psets1/population.c
@@ -6,12 +6,17 @@
 int main(void)
 {
     // Prompt the user for a starting population size
-
+    int start = get_int("Start Size: ");
     // Step 2: Make a condition with this rule: If the user enters a number less than 9 (the minimum allowed population size), the user should be re-prompted to enter a starting population size until they enter a number that is greater than or equal to 9. (If we start with fewer than 9 llamas, the population of llamas will quickly become stagnant!)
+    if ()
+        int end = get_int("End Size: ");
+
 
     // Prompt the user for an ending population size
 
     // Step 3: Make a condition with thsi rule: If the user enters a number less than the starting population size, the user should be re-prompted to enter an ending population size until they enter a number that is greater than or equal to the starting population size. (After all, we want the population of llamas to grow!)
 
-    
+    // Calculate the user input: Your program should then calculate the (integer) number of years required for the population to reach at least the size of the end value.
+
+    // Print the number of years required for the llama population to reach that end size, as by printing to the terminal Years: n, where n is the number of years.
 }
