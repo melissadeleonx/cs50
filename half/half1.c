39e7262f1c3fb221826e94c8ecaa2f0de2395821
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 19:28:37 +0000

    Tue, Oct 3, 2023, 9:28 PM +02:00

--- half/half1.c
+++ half/half1.c
@@ -6,7 +6,8 @@
 // Step 1: Make a pseudocode.
 int main(void)
 {
-    // Prompt user to enter the bill amount before ta
+    // Prompt user to enter the bill amount before tax and tip
     float Bill_before_tax_and_tip = get_float("Bill before tax and tip: ");
+    
 
 }
