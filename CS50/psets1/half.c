a7fc00b244d7d025ca60c9afc42e7e4057ca1f6b
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-03 22:04:16 +0000

    Wed, Oct 4, 2023, 12:04 AM +02:00

--- CS50/psets1/half.c
+++ CS50/psets1/half.c
@@ -15,7 +15,7 @@ int main(void)
     float bill_amount = get_float("Bill before tax and tip: ");
     float tax_percent = get_float("Sale Tax Percent: ");
     int tip_percent = get_int("Tip percent: ");
-    float half_share = half(float bill, float tax, int tip);
+    float half_share = half(bill_amount, tax_percent, tip_percent);
 
     printf("You will owe %.2f each!\n", half_share);
 }
@@ -27,8 +27,8 @@ int main(void)
 
 
 
-float half(float bill, float tax, int tip);
+float half(float bill, float tax, int tip)
 {
-   float bill_tax = bill_amount + (tax_percent/bill_amount);
-   float half = (bill_tax + (tip_percent/bill_tax)) / 2;
+   float bill_tax = bill_amount + (tax_percent/100);
+   float half = (bill_tax + (tip_percent/100)) / 2;
 }
