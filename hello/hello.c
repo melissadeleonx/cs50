b98d83eac48a2b9ea227567c13876f6b21bd2a3d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-04 09:18:43 +0000

    Wed, Oct 4, 2023, 11:18 AM +02:00

--- hello/hello.c
+++ hello/hello.c
@@ -2,9 +2,9 @@
 #include <cs50.h>
 int main(void)
 {
-    string name = get_string("What is your name? ");
-    string location = get_string("Where are you fron? ");
-    printf("hello %s from %s!\n", name, location);
+    string name = get_string("What's your name? ");
+    string location = get_string("Where are you from? ");
+    printf("hello, %s from %s!\n", name, location);
 
     return 0;
 }
