5d65b8182f1d59d9086e6d5c931f3d0c0e521029
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-04 09:43:10 +0000

    Wed, Oct 4, 2023, 11:43 AM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -9,21 +9,24 @@ int main(void)
     do
     {
         n = get_int("Size: ");
-    } while (n < 1);
+    }
+    while (n < 1);
 
     // Print . as the space then remove it later after we identify how to make the right pyramid
     for (int i = 0; i < n; i++)
     {
+        // Make the j = 1 so there is no space before the pyramid
         for (int j = 1; j < n - i; j++)
         {
             printf(" ");
         }
         // Print # NOW!!
         for (int k = 0; k < i + 1; k++)
-    {
-        printf("#");
+        {
+            printf("#");
+        }
+
+        // Print the next line outside the loop
+        printf("\n");
     }
-    printf("\n");
-    } // Moral of the story: Try and try until you succeed! Go girl!! I am not really sure how I made it happen but I guess I just invert the for loop of the i
-      // Coding and Guessing :)
 }
