5b8696eaff73a8f024ed40b0facaa0bb6aa5c6a1
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-04 12:13:24 +0000

    Wed, Oct 4, 2023, 2:13 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,12 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// Goal is to find out if a credit card is valid and identify their type(Visa, MasterCard, American Express)
+// Goal is to find out if a credit card is valid
 
 int main(void)
 {
     // Prompt user to enter the credit card number
     long number = get_number("Enter the credit card number: ");
+
+    // Identify their type(Visa, MasterCard, American Express)
 
     // Calculate checksum ??????
 
