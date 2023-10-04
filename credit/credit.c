65f1436ee898a40a23be5ffdb3aaa77d020920e9
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-04 12:27:05 +0000

    Wed, Oct 4, 2023, 2:27 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,14 +3,13 @@
 
 // Goal is to find out if a credit card is valid using Luhn’s Algorithm
 // Make functions to determine if the card provided by the user meet the requirements
-// Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
-// Add the sum to the sum of the digits that weren’t multiplied by 2.
-// If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
+// Function for Luhns Algorithm
+// Function to determine card type - American Express, MasterCard, or Visa
 
 int main(void)
 {
-    // Prompt user to enter the credit card number
-    long number = get_number("Enter the credit card number: ");
+    // Prompt user to enter the credit card number. Use get_long function
+    long number = get_long("Enter the credit card number: ");
 
     // Identify their type(Visa, MasterCard, American Express)
 
