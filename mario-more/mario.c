201884ce3efe93262d088b709a4be22b6f8ac74d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-04 09:54:49 +0000

    Wed, Oct 4, 2023, 11:54 AM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,13 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
-// Goal is to recreate Super Mario's two adjacent pyramids of blocks
 
+// Goal is to recreate Super Mario's two adjacent pyramids of blocks with space in between, combining right and left pyramid codes
 int main(void)
 
 // Prompt the user to enter the height
+// Use the do while loop to implement the limitations of the code. Use specific variables instead of i, j and k for clarity.
 {
     int height, rows, columns;
-
     do
     {
         height = get_int("Enter the height: ");
@@ -42,3 +42,41 @@ int main(void)
     }
     return 0;
 }
+
+#include <cs50.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    // Prompt user using the do while LOOP
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    } while (n < 1);
+
+    // Print the right pyramid
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i + 1; k++)
+    {
+
+        printf("#");
+    }
+     // Print the space in between using . for now then change it to space
+    printf("  ");
+
+    // Add another counter variable for the left pyramid
+        for (int l = 0; l < i + 1; l++)
+        {
+            printf("#");
+        }
+          printf("\n");
+    }
+    return 0;
+}    // Print the left pyramid, since they will all be in the same levels, the conditionals should be together with other conditionals under 1 major conditionals
