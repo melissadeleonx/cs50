522c655df35623a0a1e945490f15a12dc909065a
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-05 17:14:17 +0000

    Thu, Oct 5, 2023, 7:14 PM +02:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,16 @@
+// Design and implement a program, substitution, that encrypts messages using a substitution cipher.
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+// The program must accept a single command-line argument, the key to use for the substitution.
+int main(int argc, string argv[])
 {
+    // The key itself should be case-insensitive, so whether any character in the key is uppercase or lowercase should not affect the behavior of your program.
+
+    // Rules and error messages.
+    // If your program is executed without any command-line arguments or with more than one command-line argument
+    // If the key is invalid (as by not containing 26 characters, containing any character that is not an alphabetic character, or not containing each letter exactly once), your program should print an error message of your choice (with printf) and return from main a value of 1 immediately.
+
+
 
 }
