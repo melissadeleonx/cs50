c76070e838a383cc1b9c67e5efb3d9e4282e91e6
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-05 08:31:22 +0000

    Thu, Oct 5, 2023, 10:31 AM +02:00

--- no-vowels/novows.c
+++ no-vowels/novows.c
@@ -8,14 +8,14 @@ string replace(string argv[1]);
 int main(int argc, string argv[1])
 {
     // If command-line argument is empty, print an Error Message
-    if (argc < 2)
+    if (argc != 2)
     {
         printf("Error, please input a command-line argument.\n");
         return 1;
     }
 
     // Print the string with no vowels.
-    printf("%s\n" replace);
+    printf("%s\n" argv[]);
 }
 
 
