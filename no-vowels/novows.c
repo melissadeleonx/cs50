0a95b4db43ddc2be8d2a0fe4f0bf8317d8baea19
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-05 01:18:51 +0000

    Thu, Oct 5, 2023, 3:18 AM +02:00

--- no-vowels/novows.c
+++ no-vowels/novows.c
@@ -1,3 +1,4 @@
+// Convert a single command-line argument with no-vowel string
 #include <cs50.h>
 #include <stdio.h>
 
@@ -6,7 +7,8 @@ int main(int argc, string argv[1])
     // If command-line argument is empty, print an Error Message
     if (argc < 2)
     {
-        printf("Error, please input a command-line argument\n");
+        printf("Error, please input a command-line argument.\n");
+        return 1;
     }
 
 }
