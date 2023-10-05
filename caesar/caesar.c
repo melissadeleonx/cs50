b62434d66c4d0ae1aa1f0ef2710fe023cb1694da
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-05 16:56:49 +0000

    Thu, Oct 5, 2023, 6:56 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,13 +1,13 @@
 // Design and implement a Caesar Algorithm program, that encrypts messages using Caesar’s cipher.
 // Make sure program was run with just one command-line argument
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
 // Make a function to check that the input has only digits
-bool only_digits (string input);
+bool only_digits(string input);
 
 int main(int argc, string argv[])
 {
@@ -22,7 +22,8 @@ int main(int argc, string argv[])
     int key = atoi(argv[1]);
 
     // Convert plaintext to ciphertext and print the result
-    // Rules: Rotate the letters using the key input by the user. Implement the Caesar algorithm formula (ciphertext[i] = (plaintext[i] - initial + key) % 26 + initial)
+    // Rules: Rotate the letters using the key input by the user. Implement the Caesar algorithm formula (ciphertext[i] =
+    // (plaintext[i] - initial + key) % 26 + initial)
     string plaintext = get_string("Plaintext: ");
     string ciphertext = plaintext;
     for (int i = 0, n = strlen(plaintext); i < n; i++)
@@ -45,10 +46,7 @@ int main(int argc, string argv[])
     return 0;
 }
 
-
-
-
-bool only_digits (string input)
+bool only_digits(string input)
 {
     for (int i = 0; i < strlen(input); i++)
     {
