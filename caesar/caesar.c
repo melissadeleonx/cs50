25b52f6ddcd608375e8c824b25361314f65b7114
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-05 16:55:56 +0000

    Thu, Oct 5, 2023, 6:55 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,4 +1,4 @@
-// Design and implement a program, caesar, that encrypts messages using Caesar’s cipher.
+// Design and implement a Caesar Algorithm program, that encrypts messages using Caesar’s cipher.
 // Make sure program was run with just one command-line argument
 #include <cs50.h>
 #include <stdio.h>
@@ -22,7 +22,7 @@ int main(int argc, string argv[])
     int key = atoi(argv[1]);
 
     // Convert plaintext to ciphertext and print the result
-    // Rules: Use the formula (ciphertext[i] = (plaintext[i] - initial + key) % 26 + initial)
+    // Rules: Rotate the letters using the key input by the user. Implement the Caesar algorithm formula (ciphertext[i] = (plaintext[i] - initial + key) % 26 + initial)
     string plaintext = get_string("Plaintext: ");
     string ciphertext = plaintext;
     for (int i = 0, n = strlen(plaintext); i < n; i++)
