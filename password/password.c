c97bf39bdbe19f8a2eab8cbef8fc2967a761f18e
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-05 11:58:33 +0000

    Thu, Oct 5, 2023, 1:58 PM +02:00

--- password/password.c
+++ password/password.c
@@ -28,8 +28,7 @@ bool valid(string password)
 {
     for (int i = 0; i < strlen(password); i++)
     {
-        if (isdigit(password[i]) > 0 && (isalpha(password[i]) > 0))
-
+        if (isdigit(password[i]) && isalpha(password[i]))
         {
             return true;
         }
