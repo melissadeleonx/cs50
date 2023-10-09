f1f32152a0b3373cb9fc25ff30c698e93b9663c2
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-09 10:11:26 +0000

    Mon, Oct 9, 2023, 12:11 PM +02:00

--- CS50/lecture3/search.c
+++ CS50/lecture3/search.c
@@ -3,6 +3,18 @@
 
 int main(void)
 {
-    // Use a shorter syntax instead of saying, int numbers[7]; numbers[0] = 20;.... statically 
+    // Use a shorter syntax instead of saying, int numbers[7]; numbers[0] = 20;.... statically design
     int numbers[] = {20, 500, 10,5, 100, 1, 50}
+
+    int n = get_int("Number: ");
+    for (int = 0; i < 7; i++)
+    {
+        if (numbers[i] == n)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
