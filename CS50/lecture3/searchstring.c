517949c0075d7bd28cd74f68f6bcbe5a3f84b8ee
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-09 10:39:10 +0000

    Mon, Oct 9, 2023, 12:39 PM +02:00

--- CS50/lecture3/searchstring.c
+++ CS50/lecture3/searchstring.c
@@ -9,11 +9,11 @@ int main(void)
     string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
 
     string s = get_string("String: ");
-    // Use the string.h library to use the function strcmp, to compare strings with each other
+    // Use the string.h library to use the function strcmp, to compare strings with each other, == is not gonna compare each letter 'char' of the string so this function is required
     for (int i = 0; i < 7; i++)
     {
-         for example (strcmp(samples[i], n) == 0)
-        if (numbers[i] == n)
+        // The strings are both equal to zero which signifies 
+        if (strcmp(strings[i], n) == 0)
         {
             printf("Found\n");
             return 0;
