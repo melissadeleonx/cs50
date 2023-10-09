0dff8e17fe084da80469dc87c20ca0efc677f625
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-09 11:09:00 +0000

    Mon, Oct 9, 2023, 1:09 PM +02:00

--- CS50/lecture3/phonebook.c
+++ CS50/lecture3/phonebook.c
@@ -9,7 +9,16 @@ int main(void)
     // How to store the phone number systematically? We can combine names and numbers as strings but it is not very sophisticated
     // There is a term called 2D arrays but will be discussed in later lectures. For now, here is the solution.
     string numbers[] = {"+1-619-0000", "+1-789-0101"};
-    
 
-
+    string name = get_string("Name: ");
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
