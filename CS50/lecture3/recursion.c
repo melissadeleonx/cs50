afbb1985c6b212d2efe4ad1c5a93709e74f8ac27
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-10 10:25:42 +0000

    Tue, Oct 10, 2023, 12:25 PM +02:00

---
+++ CS50/lecture3/recursion.c
@@ -0,0 +1,9 @@
+// Pseudocode
+If there are no doors
+    Return false
+If 50 is behind middle door
+    Return true
+Else if 50 < middle door
+    Search left half
+Else if 50 > middle door
+    Search right half
