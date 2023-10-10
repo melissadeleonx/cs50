c70bd9bae73ea59ceb7bf3b5b37d8b7ee2ebca51
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-10 21:11:05 +0000

    Tue, Oct 10, 2023, 11:11 PM +02:00

--- CS50/lecture3/iteration.c
+++ CS50/lecture3/iteration.c
@@ -11,11 +11,15 @@ int main(void)
 
 void draw(int n)
 {
-    // If nothing to draw
-    if (n <= 0)
-    {
-        return;
-    }
 
-    
+// Reiterate row by row
+for (int i = 0; i < n; i++)
+    {
+        //
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
