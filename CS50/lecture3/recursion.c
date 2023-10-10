6c553fd0b870740c5c4fd945dc6faca1b9110099
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-10 10:27:30 +0000

    Tue, Oct 10, 2023, 12:27 PM +02:00

--- CS50/lecture3/recursion.c
+++ CS50/lecture3/recursion.c
@@ -7,3 +7,17 @@ Else if 50 < middle door
     Search left half
 Else if 50 > middle door
     Search right half
+
+    1  Pick up phone book
+2  Open to middle of phone book
+3  Look at page
+4  If person is on page
+5      Call person
+6  Else if person is earlier in book
+7      Open to middle of left half of book
+8      Go back to line 3
+9  Else if person is later in book
+10     Open to middle of right half of book
+11     Go back to line 3
+12 Else
+13     Quit
