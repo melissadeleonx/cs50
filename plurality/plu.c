226e10a89896872e7b517692b7fe7b4fb18e138f
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-13 07:24:05 +0000

    Fri, Oct 13, 2023, 9:24 AM +02:00

--- plurality/plu.c
+++ plurality/plu.c
@@ -72,7 +72,7 @@ bool vote(string name)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
-            candidates[i].votes++
+            candidates[i].votes++;
             return 0;
         }
     }return false;
