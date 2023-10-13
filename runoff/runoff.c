90f92619e43b34918372dbfaa5155c2ebfa8ea27
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-13 08:41:02 +0000

    Fri, Oct 13, 2023, 10:41 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,7 +127,11 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    for (int i = 0; i < )
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp())
+        return true;
+    }
     return false;
 }
 
