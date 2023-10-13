15c47f90a75a3ff4d6a05dbbdb98e522c196dba6
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-13 08:17:56 +0000

    Fri, Oct 13, 2023, 10:17 AM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -10,8 +10,7 @@ typedef struct
 {
     string name;
     int votes;
-}
-candidate;
+} candidate;
 
 // Array of candidates
 candidate candidates[MAX];
@@ -68,7 +67,7 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if(strcmp(candidates[i].name, name) == 0)
+        if (strcmp(candidates[i].name, name) == 0)
         {
             candidates[i].votes++;
             return true;
