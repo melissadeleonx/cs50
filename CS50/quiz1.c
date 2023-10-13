8deedbb77d8c7ec4111d4ab49464051c7dff3bb1
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-13 17:23:53 +0000

    Fri, Oct 13, 2023, 7:23 PM +02:00

---
+++ CS50/quiz1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void swap(int a, int b);
+
+int main(void)
+{
+    int x = 1;
+    int y = 2;
+    swap(x, y);
+    printf("%i\n", y);
+
+}
+
+void swap(int a, int b)
+{
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
