df768a44f1c9919c0d0674476fc8ff84645d1c2b
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-13 07:32:05 +0000

    Fri, Oct 13, 2023, 9:32 AM +02:00

--- plurality/plu.c
+++ plurality/plu.c
@@ -68,7 +68,7 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 1; i < candidate_count; i++)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
