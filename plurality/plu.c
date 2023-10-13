23318b3fedb4a982897172899439977e6af86d52
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-13 07:18:07 +0000

    Fri, Oct 13, 2023, 9:18 AM +02:00

--- plurality/plu.c
+++ plurality/plu.c
@@ -80,6 +80,8 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
+    int most_votes = 0;
+    
     for (int i = 0; i < voter_count; i++)
     {
         if (candidates[i].votes == )
