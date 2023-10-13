5ed2ba56751e9e4a03c5b8a655bb93cdb4434e48
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-13 16:19:51 +0000

    Fri, Oct 13, 2023, 6:19 PM +02:00

--- runoff/run.c
+++ runoff/run.c
@@ -142,18 +142,7 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    for (int i = 0; i < voter_count; i++)
-    {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            int n = preferences[i][j];
-
-            if(!candidates[n].eliminated)
-            {
-                
-            }
-        }
-    }
+    
     return;
 }
 
