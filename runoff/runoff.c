b7874c0ef5fc6a7a59edda7d8207e153be0c8761
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-13 21:07:48 +0000

    Fri, Oct 13, 2023, 11:07 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,9 +130,8 @@ bool vote(int voter, int rank, string name)
     for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(candidates[i].name, name) == 0)
-
         {
-            
+            preferences[voter][rank] = i;
             return true;
         }
     }
