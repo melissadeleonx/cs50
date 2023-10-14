1c660ad663d550d81bc515d45c538bcd751ee2db
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-14 08:01:06 +0000

    Sat, Oct 14, 2023, 10:01 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -67,7 +67,7 @@ int main(int argc, string argv[])
         printf("Maximum number of voters is %i\n", MAX_VOTERS);
         return 3;
     }
-    //* 
+    //* Voting program starts here
     // Keep querying for votes
     for (int i = 0; i < voter_count; i++)
     {
