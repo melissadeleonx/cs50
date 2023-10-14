e61a8c0fedeb6c7e9790c2bd24b2902cb20ef9ee
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-14 16:45:43 +0000

    Sat, Oct 14, 2023, 6:45 PM +02:00

--- CS50/lecture4/garbage.c
+++ CS50/lecture4/garbage.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    // If you don't initialize, garbage will come out
+    // If you don't initialize or give an explicit value for a variable, garbage values will come out
     int scores[1024];
     for (int i = 0; i < 1024; i++)
     {
