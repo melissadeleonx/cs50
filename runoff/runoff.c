86638dafed197d9ba9012f92cb724f29e25d5972
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-14 08:15:48 +0000

    Sat, Oct 14, 2023, 10:15 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -189,7 +189,10 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
+    //* Vote counts start with 0
     int min = 0;
+
+    //* 
     for (int i = 0; i < candidate_count; i++)
     {
         if (!candidates[i].eliminated && candidates[i].votes < min)
