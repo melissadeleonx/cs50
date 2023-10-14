d1c236a45eb20591c2af976dd5f42e662beb39ca
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-14 09:32:08 +0000

    Sat, Oct 14, 2023, 11:32 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -205,14 +205,12 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    bool tie = true;
-
-    for (int i = 0; i < candidate_count && tie; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
-        tie = candidates[i].eliminated || candidates[i].votes == min;
+        if (candidates[i].eliminated || candidates[i].votes == min)
         return true;
     }
-    return true;
+    return false;
 }
 
 // Eliminate the candidate (or candidates) in last place
