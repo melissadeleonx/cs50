85246189d8df8c768b2149b99d6f36c71eb1b6bb
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-14 13:16:57 +0000

    Sat, Oct 14, 2023, 3:16 PM +02:00

--- CS50/lecture4/variable.c
+++ CS50/lecture4/variable.c
@@ -5,4 +5,5 @@ int main(void)
     // Interchangeably, we can use char *s as string is really just the pointer for char data types
     char *s = "HI!";
     printf("%s\n", s);
+    printf("%p\n", &s[0]);
 }
