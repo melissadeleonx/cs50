114077a7bf913ba6fca46a9ed3da14eb5d870f3d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-14 15:57:29 +0000

    Sat, Oct 14, 2023, 5:57 PM +02:00

---
+++ CS50/lecture4/strcpy.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    // Get a string
+    char *s = get_string("s: ");
+
+    // Allocate memory for another string
+    char *t = malloc(strlen(s) + 1);
+
+    // Use the strcpy function instead of looping
+    strcpy(t, s);
+    
+    // Capitalize copy
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
+
+    // Print strings
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+}
