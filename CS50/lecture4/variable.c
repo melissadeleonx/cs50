1c77ca410dcef2164050a01141bbe10f72f8a278
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-14 12:47:51 +0000

    Sat, Oct 14, 2023, 2:47 PM +02:00

--- CS50/lecture4/variable.c
+++ CS50/lecture4/variable.c
@@ -5,7 +5,7 @@ int main(void)
 {
     string s = "HI!";
     printf("%p\n", s);
-    printf("%p\n", &s);
+    printf("%p\n", &s[0]);
     printf("%p\n", &s[1]);
     printf("%p\n", &s[2]);
     printf("%p\n", &s[3]);
