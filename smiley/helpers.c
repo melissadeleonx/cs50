1fb62b103cfc7c2c09d1b73e314775d626070c91
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-15 13:18:56 +0000

    Sun, Oct 15, 2023, 3:18 PM +02:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -1,5 +1,13 @@
 #include "helpers.h"
 
+// Let's make a pseudocode, we found this typedef struct
+// {
+    //BYTE  rgbtBlue;
+    //BYTE  rgbtGreen;
+    //BYTE  rgbtRed;
+//} __attribute__((__packed__))
+// RGBTRIPLE;
+
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
