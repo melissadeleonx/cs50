905596532201c7cf56c842adeaa27a6cdbf8a6b5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-15 12:49:14 +0000

    Sun, Oct 15, 2023, 2:49 PM +02:00

--- CS50/lecture4/scanf.c
+++ CS50/lecture4/scanf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-// Trying out alternative syntax code to make more memory allocation to the string
-#define MAX 100
+// To try out alternative syntax code for more than 1 string(words with space), we can use various libraries
 
 int main(void)
 {
     // for int and other datatype, we use & on scanf but with string it is different since string is already an address
-    char s[100];
+    char s[4];
     printf("s: ");
     scanf("%s", s);
     printf("s: %s\n", s);
