b6c2f458ba62fee08a7d924eaa1db17a810564db
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-15 12:20:44 +0000

    Sun, Oct 15, 2023, 2:20 PM +02:00

--- CS50/lecture4/scanf.c
+++ CS50/lecture4/scanf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// Trying out alternative syntax code to make more memory allocation to the string
+#define MAX 100
+
 int main(void)
 {
     // for int and other datatype, we use & on scanf but with string it is different since string is already an address
