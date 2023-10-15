9d72a9b2befefbdcd0b03d572021316e90127d05
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-15 11:26:13 +0000

    Sun, Oct 15, 2023, 1:26 PM +02:00

--- CS50/lecture4/scanf.c
+++ CS50/lecture4/scanf.c
@@ -2,8 +2,9 @@
 
 int main(void)
 {
-    char;
-    printf("x: ");
-    scanf("%i", &x);
-    printf("x: %i\n", x);
+    // for int and other datatype, we use & on scanf but with string it is different since string is already an address
+    char *s;
+    printf("s: ");
+    scanf("%s", *s);
+    printf("x: %s\n", s);
 }
