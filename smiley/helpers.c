c787fec2a4169944de5b33c90b1e0e06c76a27f5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-16 04:16:00 +0000

    Mon, Oct 16, 2023, 6:16 AM +02:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -11,7 +11,7 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Make black pixels turn red
-            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
+            if (image[i][j].rgbtRed == 0x80 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x80)
             {
                 image[i][j].rgbtRed = 0x800080;
             }
