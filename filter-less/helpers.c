78e35959c69201a5887e7be73dfecb60b2bad968
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-16 12:34:54 +0000

    Mon, Oct 16, 2023, 2:34 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -88,7 +88,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            ????????
+            
         }
     }
     return;
