8d5102ac0f53843bec9ec83d5e5a9784b958e38c
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-16 09:40:53 +0000

    Mon, Oct 16, 2023, 11:40 AM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,17 +1,16 @@
 #include "helpers.h"
 
 // Convert image to grayscale
+// RGB is same value
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+
      for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
-            {
-                image[i][j].rgbtGreen = 0xff;
-            }
+
+                image[i][j].rgbtGreen = ;
         }
     }
     return;
