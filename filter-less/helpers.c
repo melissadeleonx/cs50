c62913697940f7a739b9306bbbcf8c1e64d19328
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-16 09:05:16 +0000

    Mon, Oct 16, 2023, 11:05 AM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,16 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+     for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
+            {
+                image[i][j].rgbtGreen = 0xff;
+            }
+        }
+    }
     return;
 }
 
