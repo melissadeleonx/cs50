1217052e14dccf4c16fa0c69e938145a0a85ce75
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-17 08:56:11 +0000

    Tue, Oct 17, 2023, 10:56 AM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -89,7 +89,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            
+            // Sum of 
+            int allred = 0;
+            int allgreen = 0;
+            int allblue = 0;
+            int count = 0;
             for (int npi = i - 1; npi < i + 1; npi++)
             {
                  for each neighboring pixel from j - 1 to j + 1
