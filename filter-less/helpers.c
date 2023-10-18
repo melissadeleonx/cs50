05123cb7b477a9d27b33cb6e3a9b583c5546816a
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-18 14:54:01 +0000

    Wed, Oct 18, 2023, 4:54 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -28,10 +28,12 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            // Use the float data type and round off the answer to the nearest integer
             float sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
             float sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
             float sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
 
+            // 
             if (sepiaRed > 255)
             {
                 image[i][j].rgbtRed = 255;
