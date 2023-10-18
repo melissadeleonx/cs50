9fc5d494b8a7dfa628b9c2b94d58851a3bd10632
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-18 15:01:29 +0000

    Wed, Oct 18, 2023, 5:01 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -85,7 +85,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Use 'box blur'taking each pixel and, for each color value, giving it a new value by averaging the color values of neighboring pixels.
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Create a copy to store the blurred values
+    // Create a copy of the image to store the blurred values of the pixels
     RGBTRIPLE copy[height][width];
 
     for (int i = 0; i < height; i++)
