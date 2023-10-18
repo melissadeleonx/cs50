ed168f38703cb5c74e9e9b61b815672f79f65a23
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-18 09:02:55 +0000

    Wed, Oct 18, 2023, 11:02 AM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -9,7 +9,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width + 1; j++)
         {
-            int RGBaverage = round(image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+            int RGBaverage = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3);
 
             if (RGBaverage < 1)
             {
