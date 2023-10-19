8129f93335970985509ed4f96462a5ab7f8e47ad
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-19 17:03:25 +0000

    Thu, Oct 19, 2023, 7:03 PM +02:00

--- CS50/lecture5/listnew.c
+++ CS50/lecture5/listnew.c
@@ -2,7 +2,13 @@
 
 int main(void)
 {
+   // Array 
    int *list = malloc(3 * sizeof(int));
+
+     // Initialize list with numbers
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
 
     // Print list
     for (int i = 0; i < 3; i++)
