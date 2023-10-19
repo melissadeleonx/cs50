41785b7f01e847c21de7f2eac37cd456b5f00d05
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-19 17:08:49 +0000

    Thu, Oct 19, 2023, 7:08 PM +02:00

--- CS50/lecture5/listnew.c
+++ CS50/lecture5/listnew.c
@@ -4,8 +4,12 @@ int main(void)
 {
    // Array is a chunk of contiguous memory while malloc gives any old chunk of contiguous memory
    int *list = malloc(3 * sizeof(int));
+   if (list == NULL)
+   {
+        return 1;
+   }
 
-     // Initialize list with numbers
+    // Initialize list with numbers, you can use arrays [0][1]. . . or deference list *list but its too cryptic
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
