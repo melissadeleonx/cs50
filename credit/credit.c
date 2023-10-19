034b247ba9aef907f3da6f9b7f00c25e5097c417
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-19 08:43:54 +0000

    Thu, Oct 19, 2023, 10:43 AM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,6 +5,9 @@
 int main(void)
 {
    // Prompt for credit card number
+   long cardNumber = get_long("Number: ");
+
+   if (cardNumber )
 
    // Calculate if that checksum has a final digit that is 0 or not 0
 
