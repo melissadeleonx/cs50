1853c74709ac459f21b38077f82c43bac328ba1a
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-19 17:25:21 +0000

    Thu, Oct 19, 2023, 7:25 PM +02:00

---
+++ CS50/lecture5/listsimple.c
@@ -0,0 +1,45 @@
+// REALLOC function
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+   int *list = malloc(3 * sizeof(int));
+   if (list == NULL)
+   {
+        return 1;
+   }
+
+    // Initialize list with numbers, you can use arrays [0][1]. . . or deference list *list but its too cryptic
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+
+    int *tmp = malloc(4 * sizeof(int));
+    if (tmp == NULL)
+   {
+        free(list);
+        return 1;
+   }
+
+   for (int i = 0; i < 3; i++)
+   {
+        tmp[i] = list[i];
+
+   }
+   tmp[3] = 4;
+
+   free(list);
+
+   list = tmp;
+
+    // Print list
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i\n", list[i]);
+    }
+
+    free(list);
+    return 0;
+}
