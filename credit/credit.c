d5ac5e06f0276c8985e4838287701a1a835bc0ae
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-19 08:38:03 +0000

    Thu, Oct 19, 2023, 10:38 AM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,8 +1,15 @@
-// 
+// Credit Card Validation Program
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
+   // Prompt for credit card number
+
+   // Calculate if that checksum has a final digit that is 0 or not 0
+
+   // Check for card length and starting digits
+
+   // 
 
 }
