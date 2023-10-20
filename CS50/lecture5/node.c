377fde58da95a8f03edfdf5244a098905044f1ef
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-20 20:22:29 +0000

    Fri, Oct 20, 2023, 10:22 PM +02:00

--- CS50/lecture5/node.c
+++ CS50/lecture5/node.c
@@ -1,3 +1,4 @@
+How linked listing works in order using nodes(boxes)
 // Null means the node is empty
 node *list = NULL;
 
@@ -13,3 +14,20 @@ n->number = 1;
 n->number = NULL;
 
 list = n;
+
+// A new node is then created. Both the number and next field are both filled with garbage values.
+node *n = malloc(sizeof(node));
+
+// The number value of n’s node (the new node) is updated to 2.
+n->number = 2;
+
+// Also, the next field is updated as well.
+n->next = NULL;
+
+// Most important, we do not want to lose our connection to any of these nodes lest they be lost forever. Accordingly, n’s next field is pointed to the same memory location as list.
+n->next = list;
+
+// Finally, list is updated to point at n. We now have a linked list of two items
+list = n;
+
+
