f7a43f9a1abfee91ea45fed223d9f3470fb26778
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-20 13:10:01 +0000

    Fri, Oct 20, 2023, 3:10 PM +02:00

--- CS50/lecture5/node.c
+++ CS50/lecture5/node.c
@@ -4,6 +4,8 @@ node *list = NULL;
 // a node called n is allocated in memory
 node *n = malloc(sizeof(node));
 
-//
+// Two different syntax, same usage, the latter is easier to read
+// The number of node is assigned the value 1
 (n*).number = 1;
+n->number = 1;
 
