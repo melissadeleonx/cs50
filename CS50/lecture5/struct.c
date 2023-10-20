5bc281fbc1d7e92672296e84dd53b2be1d25c8a5
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-20 12:40:42 +0000

    Fri, Oct 20, 2023, 2:40 PM +02:00

--- CS50/lecture5/struct.c
+++ CS50/lecture5/struct.c
@@ -1,9 +1,11 @@
 // Syntax to remember,struct, ., *,  ->
 // Linked list - linking non-array data inside the memory
 
-typdef struct
+// node is a function included in C but if we want to declare it, we can add it beside typedf struct and modify the parameter of the structure inside
+typdef struct node
 {
     int number;
-    node *next;
+    struct node *next;
+
 }
 node;
