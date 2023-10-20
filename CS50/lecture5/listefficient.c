c86c9baa5185d2ceb0f19f05065a6ae8af5748cf
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-20 20:32:15 +0000

    Fri, Oct 20, 2023, 10:32 PM +02:00

--- CS50/lecture5/listefficient.c
+++ CS50/lecture5/listefficient.c
@@ -16,5 +16,14 @@ int main(int argc, char *argv[])
 
    // For each command-line argument
     for (int i = 1; i < argc; i++)
+    // Convert argument to int
+        int number = atoi(argv[i]);
+
+        // Allocate node for number
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return 1;
+        }
 
 }
