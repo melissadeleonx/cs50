75b64dc7f8a7c4eab88e0a467a31e536413451c9
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-20 20:28:38 +0000

    Fri, Oct 20, 2023, 10:28 PM +02:00

--- CS50/lecture5/listefficient.c
+++ CS50/lecture5/listefficient.c
@@ -1,45 +1,16 @@
-// Implements a list of numbers with a pointer
+// Implements Linked Listing using nodes in the code instead of malloc since it is more efficient
 #include <stdio.h>
 #include <stdlib.h>
 
+typdef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
 int main(void)
 {
-   // Array is a chunk of contiguous memory while malloc gives any old chunk of contiguous memory
-   int *list = malloc(3 * sizeof(int));
-   if (list == NULL)
-   {
-        return 1;
-   }
-
-    // Initialize list with numbers, you can use arrays [0][1]. . . or deference list *list but its too cryptic
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    int *tmp = malloc(4 * sizeof(int));
-    if (tmp == NULL)
-   {
-        free(list);
-        return 1;
-   }
-
-   for (int i = 0; i < 3; i++)
-   {
-        tmp[i] = list[i];
-
-   }
-   tmp[3] = 4;
-
-   free(list);
-
-   list = tmp;
-
-    // Print list
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%i\n", list[i]);
-    }
-
-    free(list);
-    return 0;
+   node *list = NULL;
+   
 }
