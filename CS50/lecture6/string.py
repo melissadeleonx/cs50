d38a9e1a19b4bf7fc1e573d0cf507e0c07fdaef9
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-21 12:04:15 +0000

    Sat, Oct 21, 2023, 2:04 PM +02:00

--- CS50/lecture6/string.py
+++ CS50/lecture6/string.py
@@ -1,5 +1,6 @@
 from cs50 import get_string
 
 # get_string and print, with concatenation
-answer = get_string("What's your name? ")
-print("hello, " + answer)
+# alternative to use print("hello, " + answer)
+answer = get_string("What's your name?: ")
+print(f"hello, {answer}")
