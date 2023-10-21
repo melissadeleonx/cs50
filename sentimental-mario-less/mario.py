59f7c7bb86e5fdaa58f0dc98e9d16f740179ea18
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-21 20:11:17 +0000

    Sat, Oct 21, 2023, 10:11 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -4,12 +4,8 @@ from cs50 import get_int
 
 size = get_int("Size: ")
 
-# Demonstrates while loop
+#
 i = 0
 for i in range(size):
-    j = 1
-    for j in range(size - i):
-        print(".")
-        k = 0
-        for k in range(i + 1):
-            print("#")
+       print(".")
+     print("#")
