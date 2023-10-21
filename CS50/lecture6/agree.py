f509b6f523805ad7862de94ec724d4a087a2fc94
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-21 22:35:12 +0000

    Sun, Oct 22, 2023, 12:35 AM +02:00

--- CS50/lecture6/agree.py
+++ CS50/lecture6/agree.py
@@ -2,8 +2,18 @@ s = input("Do you agree? ")
 
 #if s == 'y' or 'Y':
     #print("Agreed")
-elif s == 'n' or 'N':
+#elif s == 'n' or 'N':
+    #print("Not Agreed")
+#else:
+    #print("Please enter Y or N")
+
+if s in ["Y", "y"]:
+    print("Agreed")
+elif s in ["N", "n"]
     print("Not Agreed")
 else:
     print("Please enter Y or N")
+
+
+
 
