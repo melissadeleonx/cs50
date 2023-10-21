7b28273a283a63a2f55bb6fa0d694b6ff13feccd
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-21 11:55:17 +0000

    Sat, Oct 21, 2023, 1:55 PM +02:00

--- CS50/lecture6/hello.py
+++ CS50/lecture6/hello.py
@@ -1,3 +1,3 @@
-
- Semicolon is gone,
+# Python is a programming language that builds upon what you have already learned in C.
+# Semicolon is gone, even the commenting syntax is different from C, no \n too
 print("hello world")
