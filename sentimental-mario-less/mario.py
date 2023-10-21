fa6ea5964ba7ad2b54d66c14b376df11e876c117
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-21 19:59:45 +0000

    Sat, Oct 21, 2023, 9:59 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,6 +1,9 @@
 # TODO
+# Goal is to make right-aligned pyramid of blocks(#)
 from cs50 import get_int
 
+size = get_int("Size: ")
+
 # Demonstrates while loop
-for i in range(3):
-    print("meow")
+for i in range(size):
+    print("#")
