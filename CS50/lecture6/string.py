7073552811111b322ca2b982b1398016f13cf86f
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-21 11:56:19 +0000

    Sat, Oct 21, 2023, 1:56 PM +02:00

--- CS50/lecture6/string.py
+++ CS50/lecture6/string.py
@@ -1,6 +1,5 @@
-# get_string and print, with concatenation
-
 from cs50 import get_string
 
+# get_string and print, with concatenation
 answer = get_string("What's your name? ")
 print("hello, " + answer)
