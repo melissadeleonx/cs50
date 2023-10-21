8b4fbfdbfac3af9fee66d0e1560dd5e2ea02c98f
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-21 16:49:25 +0000

    Sat, Oct 21, 2023, 6:49 PM +02:00

--- CS50/lecture6/condition.py
+++ CS50/lecture6/condition.py
@@ -1,2 +1,12 @@
+from cs50 import get_int
+
+x = 4
+y = 5
+
+# Indention is important and should be consistent in Python. It should be done cautiously
 if x < y:
     print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equals to y")
