e398fdf3391c170dfc8156fa338994fac6f5b38d
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-21 15:03:26 +0000

    Sat, Oct 21, 2023, 5:03 PM +02:00

--- CS50/lecture6/dictionary.py
+++ CS50/lecture6/dictionary.py
@@ -3,6 +3,10 @@ words = set()
 
 def check(word):
     """Return true if word is in dictionary else false"""
+    if word in words:
+        return True
+    else:
+        return False
 
 
 def load(dictionary):
