0b29f3371dcde0898b970b9550a676869a7e4d86
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-21 22:11:22 +0000

    Sun, Oct 22, 2023, 12:11 AM +02:00

--- CS50/lecture6/agree.py
+++ CS50/lecture6/agree.py
@@ -2,9 +2,9 @@ from cs50 import get_string
 
 answer = get_string("Do you agree? ")
 
-if answer == "y":
+if answer == "y" or "Y":
     print("Agreed")
-elif answer == "n":
+elif answer == "n" or "N":
     print("Not Agreed")
 else:
     print("No answer")
