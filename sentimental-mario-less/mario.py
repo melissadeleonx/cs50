c3af4cc1e1ac0cd4a7654699893c94bf6673cecb
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-22 12:03:11 +0000

    Sun, Oct 22, 2023, 2:03 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -4,18 +4,17 @@ from cs50 import get_int
 
 def main(n):
        while True:
-        n = nt(input("Height: "))
-        draw(n)i
+        n = int(input("Height: "))
+        draw(n)
         if n > 0:
-        break
+            break
 
 def draw(n):
-
     draw(n - 1)
     for i in range(n):
         i += 1
         print("#")
 
-main(n)
+main()
 
 
