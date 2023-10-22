d47487f36e31ee72bd4e2014783b264bb009095a
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-22 12:26:41 +0000

    Sun, Oct 22, 2023, 2:26 PM +02:00

--- CS50/lecture6/mariome.py
+++ CS50/lecture6/mariome.py
@@ -1,6 +1,9 @@
 from cs50 import get_int
 
+def main()
+    height = get_height()
+    for i in range(height):
+        print("#")
 while True:
     n = get_int("Height: ")
     if n > 0:
-        break
