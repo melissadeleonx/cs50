4900b698c071afd5a7b32c07c962b1a2dff828ef
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-22 07:54:31 +0000

    Sun, Oct 22, 2023, 9:54 AM +02:00

--- CS50/lecture6/meow.py
+++ CS50/lecture6/meow.py
@@ -1,13 +1,10 @@
-meow():
-
-n = int(input("How many meow? "))
-meow(n)
-
-
-
-
-
 
 def meow(n):
     for i in range(n):
         print("meow")
+
+        
+n = int(input("How many meow? "))
+meow(n)
+
+
