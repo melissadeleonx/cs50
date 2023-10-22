73ace6e7b03a61a101f4a9c6480fa31bddbda9fb
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-22 12:31:49 +0000

    Sun, Oct 22, 2023, 2:31 PM +02:00

--- CS50/lecture6/mariome.py
+++ CS50/lecture6/mariome.py
@@ -1,9 +1,20 @@
 from cs50 import get_int
 
-def main()
+def main():
     height = get_height()
     for i in range(height):
-        print("#")
-while True:
-    n = get_int("Height: ")
-    if n > 0:
+        for j in range(height - i):
+            print("#", end="")
+    print()
+
+def get_height():
+
+    while True:
+        try:
+            n = int(input("Height: "))
+            if n > 0:
+                return n
+        except ValueError:
+            print("Not an integer")
+
+main()
