db15bd87e89b47e128f0e9c8534f42e068d21c11
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-22 12:05:12 +0000

    Sun, Oct 22, 2023, 2:05 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,19 +2,19 @@
 # Goal is to make right-aligned pyramid of blocks(#)
 from cs50 import get_int
 
+n = int(input("Height: "))
+
 def main(n):
        while True:
-        n = int(input("Height: "))
         draw(n)
         if n > 0:
             break
 
 def draw(n):
-    draw(n - 1)
     for i in range(n):
         i += 1
         print("#")
 
-main()
+main(n)
 
 
