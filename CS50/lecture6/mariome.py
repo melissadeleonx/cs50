19908acb29ab5995479b951a4d7620ff7a57504b
wisepathway <143894625+wisepathway@users.noreply.github.com>
2023-10-22 12:47:26 +0000

    Sun, Oct 22, 2023, 2:47 PM +02:00

--- CS50/lecture6/mariome.py
+++ CS50/lecture6/mariome.py
@@ -2,11 +2,7 @@ from cs50 import get_int
 
 def main():
     height = get_height()
-    for i in range(height):
-        for j in range(height - i):
-            print(".")
-            for k in range(height):
-                print("#", end="")
+    
     print()
 
 def get_height():
